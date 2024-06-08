b431f88cec84df6cb3546033fef10852d9c61265
vsulli <sullivanvalerie5@gmail.com>
2024-06-08 18:16:07 -0500

    initial commit

---
+++ 1-two-sum.py
@@ -0,0 +1,24 @@
+'''
+Two Sum
+Leetcode # 1
+# vsulli
+# 8 June 2024
+
+Given an array of integers nums, and an integer target
+return the indices of the two numbers such that they add up to 
+target
+exactly one solution, can't use same element twice
+'''
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        pass
+
+
+sol = Solution()
+
+print(sol.twoSum([2, 7, 11, 15], target = 9))
+
+print(sol.twoSum([3, 2, 4], target = 6))
+
+print(sol.twoSum([3, 3], target = 6))
