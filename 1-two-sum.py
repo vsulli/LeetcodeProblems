a3200d24fc0d2b7c727ba676f70e07a2aa131739
vsulli <sullivanvalerie5@gmail.com>
2024-06-08 18:22:06 -0500

    working solution - pretty familiar with solution

--- 1-two-sum.py
+++ 1-two-sum.py
@@ -8,11 +8,21 @@ Given an array of integers nums, and an integer target
 return the indices of the two numbers such that they add up to 
 target
 exactly one solution, can't use same element twice
+can return answer in any order
 '''
 
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        pass
+        visited_dict = {}
+        
+        for i in range(len(nums)):
+            # number you're looking for already in dict
+            if target - nums[i] in visited_dict:
+                return [i, visited_dict[target - nums[i]]]
+            else:
+                # add current num to dict
+                visited_dict[nums[i]] = i
+        
 
 
 sol = Solution()
