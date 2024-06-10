1a3a38a64226892f2a31469a9a552dcd02f2d176
vsulli <sullivanvalerie5@gmail.com>
2024-06-10 16:21:42 -0500

    make list of keys?

--- 347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements.py
@@ -13,7 +13,7 @@ You may return the answer in any order
 class Solution:
     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
         num_dict = {} # num: frequency
-
+        ans = []
         for n in nums:
             if n in num_dict:
                 num_dict[n] += 1
@@ -22,8 +22,10 @@ class Solution:
         
         # sort dictionary by value
         sorted_num_dict = {k: v for k, v in sorted(num_dict.items(), key=lambda item: item[1], reverse=True)}
-        print(sorted_num_dict)
-
+        keys_list = [key for key in sorted_num_dict]
+        for i in range(k):
+            ans.append(keys_list[i])
+        return ans
 
 sol = Solution()
 print(sol.topKFrequent(nums = [1,1,1,2,2,3], k = 2)) # [1, 2] returns the two numbers that occur most frequently
