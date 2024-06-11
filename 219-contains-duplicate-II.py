71c1de9f930fd326bd7897617a5d11b271c74580
vsulli <sullivanvalerie5@gmail.com>
2024-06-11 17:52:38 -0500

    added pseudocode and notes

--- 219-contains-duplicate-II.py
+++ 219-contains-duplicate-II.py
@@ -11,12 +11,21 @@ in the array such that nums[i] == nums[j] and abs(i - j) <= k
 
 class Solution:
     def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
-        pass
+        nearby_duplicate = False
+        # looking for number appearing twice in array
+        # difference between indices is less than or equal to k
+        # will need to loop through all numbers in nums
+        # will need a dict, check if number already exists in dict
+        # if it does, calculate difference in index
+        # if that difference = k, return True, else add to dict num:[indices]
+
+        return nearby_duplicate
 
 sol = Solution()
-print(sol.containsNearbyDuplicate(nums = [1,2,3,1], k = 3)) # true
+print(sol.containsNearbyDuplicate(nums = [1,2,3,1], k = 3)) # true (index 3 - 0 = 3)
 
-print(sol.containsNearbyDuplicate(nums = [1,0,1,1], k = 1)) # true
+print(sol.containsNearbyDuplicate(nums = [1,0,1,1], k = 1)) # true (index 3 - 2 = 1)
+# if there are multiple duplicates, how to store that info?
 
-print(sol.containsNearbyDuplicate(nums = [1,2,3,1,2,3], k = 2)) # false
+print(sol.containsNearbyDuplicate(nums = [1,2,3,1,2,3], k = 2)) # false (all indices are 3 apart)
 
