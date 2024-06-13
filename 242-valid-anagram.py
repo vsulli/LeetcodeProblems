02009f88922f4d03df21a5e544ce198c9df1b882
vsulli <sullivanvalerie5@gmail.com>
2024-06-12 19:25:40 -0500

    updated comments

--- 242-valid-anagram.py
+++ 242-valid-anagram.py
@@ -14,16 +14,17 @@ exactly once
 
 class Solution:
     def isAnagram(self, s: str, t: str)-> bool:
+        # return if strings aren't same length
         if len(s) != len(t):
             return False
         dict_s = {}
         dict_t = {}
-        # count of letters in s must match count of letters in t
+        # get count of each letter and store in dict
         for l in s:
             dict_s[l] = dict_s.get(l, 0) + 1
         for l in t:
             dict_t[l] = dict_t.get(l, 0) + 1
-        # loop through longer dictionary and check count of each?
+        # check letter count for each key
         for key in dict_s:
             if dict_s.get(key) != dict_t.get(key):
                 return False
