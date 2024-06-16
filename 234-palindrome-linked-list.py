ce29d11a40de916644259881dd2082dc5e135e2f
vsulli <sullivanvalerie5@gmail.com>
2024-06-16 09:07:45 -0500

    works, but very inefficient

--- 234-palindrome-linked-list.py
+++ 234-palindrome-linked-list.py
@@ -22,10 +22,53 @@ class Solution:
         # loop through once to get length of linked list
         # go through until halfway and then reverse linked list
         # from there and check with two pointers if they're the same
+        seen_list = []
+        isPalindrome = True
+        length = 0
+
+        curr = head
+        curr2 = ListNode(head.val)
+        while curr:
+            length +=1
+            curr = curr.next
+        if length == 1:
+            return True
+        curr = head
+        for i in range(length // 2):
+            seen_list.append(curr.val)
+            print(curr.val)
+            # get to half way point
+            curr = curr.next
+        i = len(seen_list) - 1
+        if length % 2 != 0:
+            curr = curr.next
+        while curr:
+            if seen_list[i] != curr.val:
         
+                return False
+            i -= 1
+            curr = curr.next
+
+        return isPalindrome
 
 
 
+l1 = ListNode(1)
+l2 = ListNode(2)
+l3 = ListNode(2)
+l4 = ListNode(1)
 
+l5 = ListNode(1)
+l6 = ListNode(0)
+l7 = ListNode(1)
 
+l1.next = l2
+l2.next = l3
+l3.next = l4
+
+l5.next = l6
+l6.next = l7
 sol = Solution()
+print(sol.isPalindrome(l1))
+
+print(sol.isPalindrome(l5))
