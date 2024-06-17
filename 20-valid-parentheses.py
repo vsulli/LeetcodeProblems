c68934897f34d4e03a6056cf44e8d2ca9701dae2
vsulli <sullivanvalerie5@gmail.com>
2024-06-17 17:26:29 -0500

    slightly faster solution

--- 20-valid-parentheses.py
+++ 20-valid-parentheses.py
@@ -15,22 +15,19 @@ Every close bracket has a corresponding open bracket of the same type.
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        valid = True
-        symbols = {')': '(', '}': '{', ']': '['} # hashmap of corr symbol
-        # loop through adding to stack?
-        # when you reach right symbol, 
-        # pop from stack and match to symbol value
-            # if they don't match then return False
-        l_stack = []
-        for c in s:
-            if c not in symbols:
-                l_stack.append(c)
-            else:
-                if not l_stack:
-                    return False
-                l_symbol = l_stack.pop()
-                if l_symbol != symbols.get(c):
-                    return False
-        if l_stack:
-            return False
-        return valid
+        # Create a pair of opening and closing parrenthesis...
+        opcl = dict(('()', '[]', '{}'))
+        # Create stack data structure...
+        stack = []
+        # Traverse each charater in input string...
+        for idx in s:
+            # If open parentheses are present, append it to stack...
+            if idx in '([{':
+                stack.append(idx)
+            # If the character is closing parentheses, check that the same type opening parentheses is being pushed to the stack or not...
+            # If not, we need to return false...
+            elif len(stack) == 0 or idx != opcl[stack.pop()]:
+                return False
+        # At last, we check if the stack is empty or not...
+        # If the stack is empty it means every opened parenthesis is being closed and we can return true, otherwise we return false...
+        return len(stack) == 0
