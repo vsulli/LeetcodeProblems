99bbefb9b048c63ed7bf86dd0edb9336f019513c
vsulli <sullivanvalerie5@gmail.com>
2024-06-18 18:03:11 -0500

    doesn't account for if denom is equal to 0

--- 1232-check-if-straight-line.py
+++ 1232-check-if-straight-line.py
@@ -18,16 +18,17 @@ class Solution:
         # undefined line - division by 0, check if all 0's and return true?
         # formula for slope = rise / run 
         old_denom = (coordinates[1][0] - coordinates[0][0]) # diff x values
-    
+        
         if old_denom > 0:
             old_slope = ((coordinates[1][1] - coordinates[0][1]) / old_denom) 
-       
+            
         for i in range(1, len(coordinates)):
             denom = (coordinates[i][0] - coordinates[i-1][0]) # diff x values
             if denom > 0:
                 slope = ((coordinates[i][1] - coordinates[i-1][1]) / denom) 
-                      
-            if denom != old_denom or (denom > 0 and slope != old_slope):
+                  
+            if denom > 0 and slope != old_slope:
+
                 return False
             
             
@@ -37,9 +38,11 @@ class Solution:
 
 sol = Solution()
 
-
 print(sol.checkStraightLine(coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]])) # true
 
 print(sol.checkStraightLine(coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]])) # false
 
 print(sol.checkStraightLine(coordinates = [[0,0],[0,1],[0,-1]])) # true
+
+print(sol.checkStraightLine(coordinates = [[-3,-2],[-1,-2],[2,-2],[-2,-2],[0,-2]])) # true
+# slope of 0
