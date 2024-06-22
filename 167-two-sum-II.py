98fa5bc553787e67adf13a021e07e9fc0368c97e
vsulli <sullivanvalerie5@gmail.com>
2024-06-22 11:11:08 -0500

    loop through rest of array incrementing right pointer while sum too low

---
+++ 167-two-sum-II.py
@@ -0,0 +1,37 @@
+'''
+Two Sum II - Input Array is Sorted
+Leetcode # 167
+vsulli
+22 June 2024
+
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+Your solution must use only constant extra space.
+'''
+
+class Solution:
+    def twoSum(self, numbers: list[int], target: int) -> list[int]:
+        pass
+        # no extra storing of nums and sum
+        # sorted, so if sum is too low, need to increment right pointer?
+        # nested for loop?
+        j = 1
+        for i in range(len(numbers)):
+            while numbers[i] + numbers[j] < target:
+                j+=1
+            if j < len(numbers) and numbers[i] + numbers[j] == target:
+                return [i+1, j+1]
+            j += 1
+
+sol = Solution()
+
+print(sol.twoSum(numbers = [2,7,11,15], target = 9)) # [1, 2]
+
+
+print(sol.twoSum(numbers = [2,3,4], target = 6)) # [1, 3]
+
+print(sol.twoSum(numbers = [-1,0], target = -1)) # [1,2]
