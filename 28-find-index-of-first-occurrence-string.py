ee114c41e9e4e6eed10f1d371fc9f84048143ba5
vsulli <sullivanvalerie5@gmail.com>
2024-06-21 20:01:03 -0500

    working solution

--- 28-find-index-of-first-occurrence-string.py
+++ 28-find-index-of-first-occurrence-string.py
@@ -12,18 +12,14 @@ if needle is not part of haystack
 
 class Solution:
     def strStr(self, haystack: str, needle: str)-> int:
-        # return index of start of the word
-        
-        # loop through whole string
-        # if you encounter first letter of needle, store index
-        # if you loop through entire part of string and all letters there
-        # return index
-        # else keep looking for word in rest of str
-        return haystack.find(needle)
+        for i in range(len(haystack)-len(needle)+1):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1
 
 sol = Solution()
 
-'''
+
 print(sol.strStr(haystack= "sadbutsad", needle = "sad")) # index 0
 
 print(sol.strStr(haystack="leetcode", needle="leeto")) # -1
@@ -31,5 +27,5 @@ print(sol.strStr(haystack="leetcode", needle="leeto")) # -1
 print(sol.strStr(haystack="goodcar", needle="car")) # 4
 
 print(sol.strStr(haystack="cargood", needle="car")) # 0
-'''
+
 print(sol.strStr(haystack="mississippi", needle="issip")) #4
