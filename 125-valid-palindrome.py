83140250642c835b3530bcab034364c321a6e905
vsulli <sullivanvalerie5@gmail.com>
2024-06-22 10:55:13 -0500

    neetcode solution 2 using ascii value comparison

--- 125-valid-palindrome.py
+++ 125-valid-palindrome.py
@@ -16,14 +16,32 @@ or false otherwise.
 
 import string
 
+
 class Solution:
+    def alphaNum(self, c):
+        # check if ascii value falls within range for upper letter,
+        # lower letter, or number
+        return (ord('A') <= (ord(c)) <= ord('Z') or
+                ord('a') <= (ord(c)) <= ord('z') or
+                ord('0') <= (ord(c)) <= ord('9'))
+
     def isPalindrome(self, s: str) -> bool:
-        newStr = ""
-        for c in s:
-            if c.isalnum():
-                newStr += c.lower()
-        
-        return newStr == newStr[::-1]
+        l, r = 0, len(s) - 1
+
+        # while pointers haven't met
+        while l < r:
+            # increment left pointer if necessary and make sure doesn't
+            # pass right pointer
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while r > l and not self.alphaNum(s[r]):
+                r -=1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+        return True
+
+
 
 sol = Solution()
 
