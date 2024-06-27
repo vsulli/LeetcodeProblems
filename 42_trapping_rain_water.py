d2494e3ebe122c3efbf6aff5d94073d1537b65ab
vsulli <sullivanvalerie5@gmail.com>
2024-06-26 19:11:54 -0500

    neetcode solution

--- 42_trapping_rain_water.py
+++ 42_trapping_rain_water.py
@@ -11,27 +11,23 @@ it can trap after raining
 
 class Solution:
     def trap(self, height: list[int]) -> int:
-        # two pointers l and r
-        # in order to count as water container, must have columns left and right of it of at least equal height
-        # check slice between larger columns to subtract out  squares? or 
-        # just subtract that out
+        # O(1) memory solution
+        if not height: return 0
 
-        # initialize pointer to 0 and 2?
-        # if len(height) < 3 return 0
-
-        water_contained = 0
-        
         l, r = 0, len(height) - 1
-        if height[l] == 0:
-            l +=1
-        if height[r] == 0:
-            r -=1
-        # while l < r:
-        # water contained is the height of lowest column * difference in index
-        # subtract sliced sum of inbetween indices
-        water = (min(height[l], height[r]) * (r - l)) - 1 - sum(i for i in range(l+1, r) if height[i] == 1)
-        print(water)
+        leftMax, rightMax = height[l], height[r]
+        res = 0
 
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
+        return res
 
 
 sol = Solution()
