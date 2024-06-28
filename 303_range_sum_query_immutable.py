5942477ddec330b637f8f8633c73f43bce673408
vsulli <sullivanvalerie5@gmail.com>
2024-06-27 20:01:18 -0500

    getting the prefix sum for whole row

---
+++ 303_range_sum_query_immutable.py
@@ -0,0 +1,45 @@
+'''
+Range Sum Query - Immutable
+Leetcode # 303
+vsulli
+27 June 2024
+
+Given an integer array nums, handle multiple 
+queries of the following type:
+
+Calculate the sum of the elements of 
+nums between indices left and right inclusive where 
+left <= right.
+Implement the NumArray class:
+
+NumArray(int[] nums) Initializes the object with 
+the integer array nums.
+int sumRange(int left, int right) Returns the sum 
+of the elements of nums between indices left and 
+right inclusive (i.e. nums[left] + nums[left + 1] 
++ ... + nums[right]).
+ 
+'''
+
+class NumArray:
+
+    def __init__(self, nums: list[int]):
+        self.nums = nums
+
+    def sumRange(self, left: int, right: int) -> int:
+        prefix_sum = [0] * len(self.nums)
+        total = 0
+        for i in range(len(self.nums)):
+            total += self.nums[i]
+            prefix_sum[i] = total
+        print(prefix_sum)
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
+
+obj = NumArray([-2, 0, 3, -5, 2, -1])
+print(obj.sumRange(0, 2)) # 1
+print(obj.sumRange(2, 5)) # -1
+print(obj.sumRange(0, 5)) # -3
