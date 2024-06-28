a54c2517d776f7de4c7d842d3e519c02e52929b4
vsulli <sullivanvalerie5@gmail.com>
2024-06-28 18:31:46 -0500

    neetcode solution

--- 304_range_sum_query_2d_immutable.py
+++ 304_range_sum_query_2d_immutable.py
@@ -28,16 +28,22 @@ works on O(1) time complexity.
 class NumMatrix:
 
     def __init__(self, matrix: list[list[int]]):
-        self.matrix = matrix
-        # create prefix sum matrix?
+        self.sum_ = [[0] * (len(matrix[0]) +1) for _ in range(len(matrix) + 1)]
+        for i, line in enumerate(matrix):
+            previous = 0
+            for j, num in enumerate(line):
+                previous += num
+                above = self.sum_[i][j + 1]
+                self.sum_[i+1][j+1] = previous + above
         
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        pass
-        # will have to initialize a dummy row above top row and to left?
+        sum_col2 = self.sum_[row2 + 1][col2 + 1] - self.sum_[row1][col2 + 1]
+        sum_col1 = self.sum_[row2 + 1][col1] - self.sum_[row1][col1]
+        return sum_col2 - sum_col1  
 
 
 
 # Your NumMatrix object will be instantiated and called as such:
 obj = NumMatrix([[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]])
-print(obj.sumRegion(2,1,4,3))
+print(obj.sumRegion(2,1,4,3)) # red square
