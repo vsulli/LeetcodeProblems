2c18f8dbf7ba4e78fae4407d0f7c8894c7cd132f
vsulli <sullivanvalerie5@gmail.com>
2024-06-28 19:13:29 -0500

    not getting right sum amount

--- 724_find_pivot_index.py
+++ 724_find_pivot_index.py
@@ -24,36 +24,34 @@ such index exists, return -1.
 
 class Solution:
     def pivotIndex(self, nums: list[int]) -> int:
-        # need left sum to be equal to right sum
-        # if index is 0 then left sum is 0
-        # if index reaches length of nums, return -1
+
         l_sum = 0
         r_sum = 0
         for i in range(len(nums)):
-            # left or right edges
             if i == 0:
-                l_sum = 0
-                r_sum = sum(nums[i+1:])
-                if l_sum == r_sum:
-                    return i
-            elif i == len(nums) -1:
-                l_sum = sum(nums[0:i])
-                r_sum = 0
-                if l_sum == r_sum:
-                    return i
+                if sum(nums[i+1:]) == 0:
+                    return 0
+            elif i == len(nums) - 1:
+                if sum(nums[0:i]) == 0:
+                    return 0
             else:
-                l_sum = sum(nums[0:i])
-                r_sum = sum(nums[i+1:])
-                if l_sum == r_sum:
+                nums[i-1] = l_sum + nums[i-1]
+                if nums[i-1] == sum(nums[i+1:]):
                     return i
+                print(nums[i-1])
         return -1
+
+
+        # get prefix sum?
+
             
 sol = Solution()
 print(sol.pivotIndex(nums = [1,7,3,6,5,6])) # 3
 
-
+'''
 
 print(sol.pivotIndex(nums = [1,2,3])) # -1
 
 print(sol.pivotIndex(nums = [2,1,-1])) # 0
 
+'''
