032523159ac7d2e8b2df4ddbcc74ee597e5906d8
vsulli <sullivanvalerie5@gmail.com>
2024-07-01 20:14:01 -0500

    working solution

--- 350_intersection_of_two_arrays_II.py
+++ 350_intersection_of_two_arrays_II.py
@@ -14,24 +14,16 @@ may return the result in any order.
 
 class Solution:
     def intersect(self, nums1: list[int], nums2: list[int]) -> list[int]:
-        nums1 = sorted(nums1)
-        nums2 = sorted(nums2)
-        # answer will contain numbers in both, 
-        # if appears multiple times in both arrays then answer will have that too
-        # result can be in any order
-        # sort nums first
-        # keep count of current number
-        # if in one, but not other, skip
-        # only matters length of shortest array
-        ans = []
-        for n in min(nums1, nums2, key = len):
-            if n in nums1 and n in nums2:
-                ans.append(n)
+        arr = [0] * 1001
+        result = []
+        for n in nums1:
+            arr[n] +=1
 
-
-        
-
-        return ans
+        for n in nums2:
+            if arr[n] > 0:
+                result.append(n)
+                arr[n] -= 1
+        return result[: len(result)]
 
 sol = Solution()
 
@@ -40,4 +32,4 @@ print(sol.intersect(nums1 = [1,2,2,1], nums2 = [2,2])) # [2,2]
 
 print(sol.intersect(nums1 = [4,9,5], nums2 = [9,4,9,8,4])) # [4,9]
 
-print(sol.intersect(nums1 = [4,4,4], nums2 = [9,4,9,8,4])) # [4,9]
+print(sol.intersect(nums1 = [4,4,4], nums2 = [9,4,9,8,4])) # [4,4]
