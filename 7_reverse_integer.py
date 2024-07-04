419f298b91dff79e5af89a5057657cbc5db02008
vsulli <sullivanvalerie5@gmail.com>
2024-07-04 08:02:50 -0500

    working solution

--- 7_reverse_integer.py
+++ 7_reverse_integer.py
@@ -14,28 +14,29 @@ assume the environment doesn't allow you to store
 64 bit integers  signed or unsigned
 '''
 
+import math
+
 class Solution:
     def reverse(self, x: int) -> int:
 
-        num = abs(x)
-        if num < 9: 
-            return x
-        
-        res = 0
+        MAX_INT = 2 ** 31 - 1
+        MIN_INT = -2 ** 31
+        reverse = 0
 
-        hundreds = (num % 10) * 100
+        while x != 0:
+            # causes value to go outside range return 0
+            if reverse > MAX_INT / 10 or reverse < MIN_INT / 10:
+                return 0
+            
+            digit = x % 10 if x > 0 else x % -10
+            reverse = reverse * 10 + digit
+            x = math.trunc(x / 10)
 
-        tens = ((num // 10) % 10) * 10
-
-        ones = ((num // 100) % 10) 
-
-        res = hundreds + tens + ones
-
-        return -res if x < 0 else res
+        return reverse
 
 
 sol = Solution()
-'''
+
 
 print(sol.reverse(x = 123)) 
 
@@ -44,6 +45,5 @@ print(sol.reverse(x = -123))
 print(sol.reverse(x = 120))
 
 print(sol.reverse(x = 1))
-'''
 
-print(sol.reverse(x = 10))
+print(sol.reverse(x = 75))
