5cfe3b84db0459c1f0d3cdd99fbbb658f7ff8d1f
vsulli <sullivanvalerie5@gmail.com>
2024-07-06 15:16:35 -0500

    should be a recursive solution?

--- 22_generate_parentheses.py
+++ 22_generate_parentheses.py
@@ -12,15 +12,36 @@ well-formed parentheses.
 
 class Solution:
     def generateParenthesis(self, n: int) -> list[str]:
-        pass
+        # left
+        # right
+        ans = []
+        # lined up
+        ans.append("()" * n)
+        # inside
+        string = "("
+        for i in range(n -1):
+            string += "()"
+        string += ")"
+        if string not in ans:
+            ans.append(string)
+
+        
+
+        return ans
     # need to generate all combinations of well-formed parentheses
     # 5 combinations with 3
     # n will be 8 at most
-    
+
+    # 1 -- ()
+    # 2 -- ()(), (())
+    # 3 -- ()()(), (()()), ((())), (())(), ()(())
+    # 4 -- ()()()(), (()()()), (((()))), ((()))(), ()((())), (())(())
 
 sol = Solution()
 
 print(sol.generateParenthesis(n = 3))
+
+print(sol.generateParenthesis(n = 2))
 
 print(sol.generateParenthesis(n = 1))
 
