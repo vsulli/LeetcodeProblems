d6b8a41cc18001ea3f04c661b1a0325cd33f135b
vsulli <sullivanvalerie5@gmail.com>
2024-07-06 15:04:21 -0500

    problem setup

---
+++ 22_generate_parentheses.py
@@ -0,0 +1,26 @@
+'''
+Pass the Pillow
+Leetcode # 22
+vsulli
+6 July 2024
+
+Given n pairs of parentheses, write a function 
+to generate all combinations of 
+well-formed parentheses.
+
+'''
+
+class Solution:
+    def generateParenthesis(self, n: int) -> list[str]:
+        pass
+    # need to generate all combinations of well-formed parentheses
+    # 5 combinations with 3
+    # n will be 8 at most
+    
+
+sol = Solution()
+
+print(sol.generateParenthesis(n = 3))
+
+print(sol.generateParenthesis(n = 1))
+
