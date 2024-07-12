6564486bca02465232e6f5679b7fa2cbc45e2dc7
vsulli <sullivanvalerie5@gmail.com>
2024-07-11 20:54:05 -0500

    use current index as 3rd pointer

--- 16_3sum_closest.py
+++ 16_3sum_closest.py
@@ -20,25 +20,38 @@ would have exactly one solution.
 class Solution:
     def threeSumClosest(self, nums: list[int], target: int) -> int:
         # sort nums
-        # start as 2 sum
-        # then iterate through remaining?
-        sorted_nums = sorted(nums)
-        closest_sum = sum(nums[0:4])
-        l = 0
-        r = len(nums) - 1
-        m = int(len(nums) / 2) 
-        while l <  r:
-            print("mid: " + str(m))
-            current_sum = nums[l] + nums[r] + nums[m]
-            if abs(target - current_sum) < abs(target - closest_sum):
-                closest_sum = current_sum
-            
-            if current_sum > target:
-                r -= 1
-            elif current_sum < target:
-                l += 1
+        # 3 pointers
+        # check if current sum is closer than stored closest sum
+
+    
+        nums.sort()
+        # intialize to infinity
+        closest_sum = float('inf')
+
+        # never have to go to end of nums array due to left and right pointers
+        for i in range(len(nums) -2):
+            l = i + 1
+            r = len(nums) - 1
+
+            # check left and right point of index
+            # loop while left less than right
+            while l < r:
+                total = nums[i] + nums[l] + nums[r]
+
+                if total == target: 
+                    return total
+                
+                # check if current total less than diff of stored sum
+                if abs(total - target) < abs(closest_sum - target):
+                    closest_sum = total
+                
+                # move left pointer right
+                if total < target:
+                    l += 1
+                else:
+                    r -= 1
+                    
         return closest_sum
-            
 
 
 sol = Solution()
