ce7d57c00cd9d240a4cec764b67b5c9144f1a71c
vsulli <sullivanvalerie5@gmail.com>
2024-07-13 14:28:54 -0500

    solution for right-angled number pyramid

--- practice.py
+++ practice.py
@@ -23,5 +23,21 @@ def rightTriangle(n: int):
                 print('*',end=' ')
             print('')
 
+'''
 rightTriangle(3)
 rightTriangle(6)
+'''
+
+# Pattern-3: Right-Angled Number Pyramid
+
+def numberPyramid(n: int):
+    num = 1
+    for i in range(n):
+        for j in range(i+1):
+            print(num, end=' ')
+            num += 1
+        print('')
+        num = 1
+
+numberPyramid(3)
+numberPyramid(6)
