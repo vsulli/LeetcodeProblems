714881baa8b8e3e362a5ef6c5cb0ccc0d22d56f5
vsulli <sullivanvalerie5@gmail.com>
2024-07-13 14:21:58 -0500

    python two nested loops

--- practice.py
+++ practice.py
@@ -18,10 +18,10 @@ starPattern(6)
 # Pattern-2: Right-Angled Triangle Pattern
 
 def rightTriangle(n: int):
-    star_count = 1
-    for i in range(n):
-        print("*" * star_count)
-        star_count += 1
+    for i in range(0,n):
+            for j in range(i+1):
+                print('*',end=' ')
+            print('')
 
 rightTriangle(3)
 rightTriangle(6)
