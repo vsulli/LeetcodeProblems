eeaec2702aff1a85204a1bbc24f2d52b1a70a668
vsulli <sullivanvalerie5@gmail.com>
2024-07-13 07:23:30 -0500

    doesn't take into account if max is from width

--- 84_largest_rectangle_in_histogram.py
+++ 84_largest_rectangle_in_histogram.py
@@ -13,10 +13,25 @@ rectangle in the histogram.
 
 class Solution:
     def largestRectangleArea(self, heights: list[int]) -> int:
-        pass
+        # given heights, each width is 1
+        # need to loop through 
+        # indices that are adjacent will take the lowest of adjacent
+        # to calculate area
+        max_area = max(heights)
+
+        for i in range(len(heights)-1):
+            j = i+1
+            area = min(heights[i], heights[j]) * 2
+            max_area = max(area, max_area)
+        return max_area
+
+
+
 
 sol = Solution()
 
 print(sol.largestRectangleArea(heights = [2,1,5,6,2,3])) # 10
 
 print(sol.largestRectangleArea(heights = [2,4])) # 4
+
+print(sol.largestRectangleArea(heights = [0,9])) # 9
