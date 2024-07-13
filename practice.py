a74d81d15ac97d8e37d529281ec2a263770c5ee7
vsulli <sullivanvalerie5@gmail.com>
2024-07-13 14:19:02 -0500

    one solution for right-angled triangle pattern

--- practice.py
+++ practice.py
@@ -9,6 +9,19 @@ def starPattern(n: int):
         print("*" * n)
 
 
+'''
 starPattern(3)
 
 starPattern(6)
+'''
+
+# Pattern-2: Right-Angled Triangle Pattern
+
+def rightTriangle(n: int):
+    star_count = 1
+    for i in range(n):
+        print("*" * star_count)
+        star_count += 1
+
+rightTriangle(3)
+rightTriangle(6)
