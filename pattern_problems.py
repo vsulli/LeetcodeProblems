5a30b5628d2cee471323d6a1163a84c608ffeb6f
vsulli <sullivanvalerie5@gmail.com>
2024-07-13 14:39:13 -0500

    inverted triangle stars solution

--- pattern_problems.py
+++ pattern_problems.py
@@ -54,6 +54,21 @@ def numberPyramidII(n: int):
         num += 1
         print("")
                
-
+'''
 numberPyramidII(3)
 numberPyramidII(6)
+'''
+
+# Pattern-5: Inverted Right Pyramid
+'''
+* * *
+* * 
+*
+'''
+
+def invertedPyramid(n: int):
+    for num in range(n, 0, -1):
+        print("*" * num)
+
+invertedPyramid(3)
+invertedPyramid(6)
