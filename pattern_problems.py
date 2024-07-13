7749f81760e2652bcdc65a62edb2d26fef72e02f
vsulli <sullivanvalerie5@gmail.com>
2024-07-13 16:54:17 -0500

    half diamond solution using loop

--- pattern_problems.py
+++ pattern_problems.py
@@ -195,13 +195,13 @@ diamondStar(6)
 '''
 
 def halfDiamondStar(n: int):
-    star = 1
-    for i in range(n-1):
-        print("*" * star)
-        star += 1
-    for j in range(n - 1, -1, -1):
-        print("*" * star)
-        star -= 1
+
+    for i in range(1, n+1):
+        print("*" * i)
+
+    for j in range(n - 1, 0, -1):
+        print("*" * j)
+
 
 halfDiamondStar(3)
 halfDiamondStar(6)
