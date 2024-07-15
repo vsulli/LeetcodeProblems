3fb74a12cff2e851151f1130512cc712b6a250f1
vsulli <sullivanvalerie5@gmail.com>
2024-07-15 18:35:08 -0500

    fibonacci sequence

--- 70_climbing_stairs.py
+++ 70_climbing_stairs.py
@@ -12,13 +12,18 @@ Each time you can either climb
 ways can you climb to the top?
 '''
 
+def fib(n):
+        if n <= 1:
+            return n
+        return fib(n-1) + fib(n-2)
+
 class Solution:
+    
     def climbStairs(self, n: int) -> int:
         # need a recursive answer becuase once you check one combo 
         # you need to check all others
-        
-
-
+        return fib(n + 1)
+        # ways to climb(n) = ways(n - 1) + ways( n - 2)
         
 
 
