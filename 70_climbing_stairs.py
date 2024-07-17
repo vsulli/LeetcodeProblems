1f92f1bf0eb7cc5e8b35fd8818b3974e478ddc7c
vsulli <sullivanvalerie5@gmail.com>
2024-07-16 19:06:43 -0500

    more efficient solution

--- 70_climbing_stairs.py
+++ 70_climbing_stairs.py
@@ -12,13 +12,7 @@ Each time you can either climb
 ways can you climb to the top?
 '''
 
-from functools import lru_cache
 
-@lru_cache(maxsize = 2)
-def fib(n):
-    if n < 2:
-        return n
-    return fib(n-1) + fib(n-2)
 
 class Solution:
     
@@ -26,7 +20,13 @@ class Solution:
         # ways(n) = ways(n-1) + ways(n-2)
         # always the sum of the two previous
 
-        return fib(n+1)
+        dp = [0]*(n+2)
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
        
 
         
