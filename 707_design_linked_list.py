01fb037f4ec37ea83f89ad4bdfd0d597bb99ba70
vsulli <sullivanvalerie5@gmail.com>
2024-07-21 10:33:59 -0500

    initial commit

---
+++ 707_design_linked_list.py
@@ -0,0 +1,46 @@
+''' 
+Design Linked List
+Leetcode # 707
+vsulli
+21 July 2024
+
+Design your implementation of the linked list. 
+You can choose to use a singly or doubly linked list.
+A node in a singly linked list should have 
+two attributes: val and next. val is the value 
+of the current node, and next is a pointer/reference 
+to the next node.
+If you want to use the doubly linked list, you will 
+need one more attribute prev to indicate the previous 
+node in the linked list. Assume all nodes in the 
+linked list are 0-indexed.
+'''
+
+class MyLinkedList:
+
+    def __init__(self):
+        
+
+    def get(self, index: int) -> int:
+        
+
+    def addAtHead(self, val: int) -> None:
+        
+
+    def addAtTail(self, val: int) -> None:
+        
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        
+
+    def deleteAtIndex(self, index: int) -> None:
+        
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+# obj = MyLinkedList()
+# param_1 = obj.get(index)
+# obj.addAtHead(val)
+# obj.addAtTail(val)
+# obj.addAtIndex(index,val)
+# obj.deleteAtIndex(index)
