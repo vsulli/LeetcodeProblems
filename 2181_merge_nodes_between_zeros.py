0763e108f00f9bff461da8ddee858bcec7d7e124
vsulli <sullivanvalerie5@gmail.com>
2024-07-27 09:34:02 -0500

    editorial solution - learn this version

--- 2181_merge_nodes_between_zeros.py
+++ 2181_merge_nodes_between_zeros.py
@@ -18,24 +18,24 @@ class Solution:
             curr = curr.next
 
     def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # BASE CASE -> if we have a single zero, simply return null
-        if not head.next:
-            return None
-        
-        # fetch sum from current 0 to next 0
-        ptr = head.next
-        sum = 0
-        while ptr.val != 0:
-            sum += ptr.val
-            ptr = ptr.next
-        
-        # assign sum on the first node between nodes having value 0
-        head.next.val = sum
-        
-        # call and get the answer and connect the answer to next of head->next
-        head.next.next = self.mergeNodes(ptr)
-        
-        # return head->next..=> new head
+        # Initialize a sentinel/dummy node with the first non-zero value.
+        modify = head.next
+        next_sum = modify
+
+        while next_sum:
+            sum = 0
+            # Find the sum of all nodes until you encounter a 0.
+            while next_sum.val != 0:
+                sum += next_sum.val
+                next_sum = next_sum.next
+
+            # Assign the sum to the current node's value.
+            modify.val = sum
+            # Move nextSum to the first non-zero value of the next block.
+            next_sum = next_sum.next
+            # Move modify also to this node.
+            modify.next = next_sum
+            modify = modify.next
         return head.next
 
 
