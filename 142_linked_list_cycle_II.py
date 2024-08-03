b661a308274238e51f243b94ca64f9712e8c472c
vsulli <sullivanvalerie5@gmail.com>
2024-08-03 08:20:39 -0500

    initial commit for new problem

---
+++ 142_linked_list_cycle_II.py
@@ -0,0 +1,52 @@
+'''
+Find the Duplicate Number
+Leetcode # 2678
+vsulli
+2 August 2024
+
+Given an array of integers nums 
+containing n + 1 integers where 
+each integer is in the range 
+[1, n] inclusive.
+
+There is only one repeated number 
+in nums, return this repeated number.
+
+You must solve the problem without 
+modifying the array nums and uses 
+only constant extra space.
+'''
+
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # first use Floyd's Algorithm to detect a cycle with fast and slow pointers
+            # if no cycle detected, return None
+
+
+        # then create a second slow pointer starting at the beginning and
+        # where both slow pointers meet is the intersection
+
+
+
+sol = Solution()
+
+n1 = ListNode(3)
+n2 = ListNode(2)
+n3 = ListNode(0)
+n4 = ListNode(-4)
+
+n1.next = n2
+n2.next = n3
+n3.next = n4
+n4.next = n2
+
+print(sol.detectCycle(n1))
