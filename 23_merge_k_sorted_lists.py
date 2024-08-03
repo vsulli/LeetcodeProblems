f370658417a49925d15b4c14858d5ac4120ffca7
vsulli <sullivanvalerie5@gmail.com>
2024-08-03 18:24:41 -0500

    neetcode solution

--- 23_merge_k_sorted_lists.py
+++ 23_merge_k_sorted_lists.py
@@ -22,32 +22,40 @@ class ListNode:
         self.next = next
 
 class Solution:
+
     def mergeKLists(self, lists: list[Optional[ListNode]]) -> Optional[ListNode]:
-        # make recursive?
-        # sort between two lists and then while there is another linked list in array
-        # that hasn't been visited sort?
-        if len(lists) == 1:
-            return lists[0]
+        # O(nlogk) 
+
+        if not lists or len(lists) == 0:
+            return None
         
-        pointer = 1 # keep track of which other list is being sorted into main one
+        # take pairs of linked lists and merge them
+        # until only one output
+        while len(lists) > 1:
+            mergedLists = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+                mergedLists.append(self.mergeList(l1, l2))
+            lists = mergedLists
+        return lists[0]
+    
+    def mergeList(self, l1, l2):
+        dummy = ListNode()
+        tail = dummy
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
 
-        dummy = ListNode(-1)
-        dummy_p = dummy
-        curr = lists[0]
-        second = lists[pointer]
-
-        while curr.next:
-            if curr.val <= second.val:
-                dummy_p.next = ListNode(curr.val)
-                curr = curr.next
-                dummy_p = dummy_p.next
             else:
-                dummy_p.next = ListNode(second.val)
-                second = second.next
-                dummy_p = dummy_p.next
-
-        
-        
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        if l1:
+            tail.next = l1
+        if l2: 
+            tail.next = l2
         return dummy.next
 
 sol = Solution()
