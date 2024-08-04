05b35e96f4d28c0fe5ed471e19d4c6d88dc42258
vsulli <sullivanvalerie5@gmail.com>
2024-08-04 16:49:25 -0500

    reviewing previous problem

--- practice.py
+++ practice.py
@@ -1,11 +1,28 @@
-nums = [1,2,3,4]
+# Review
+# 347 - Top K Frequent Elements
 
-array = []
-j = 0
-for i in range(0, len(nums)):
-	array.append(sum(nums[:i+1]))
-	for j in range(i, len(nums)):
-		array.append(sum(nums[:j+1]))
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        ans = []
+        res = [0] * (max(nums)+1)
+        for n in set(nums):
+            count = nums.count(n)
+            res[n] = count
+		
+        # need to return indices for k most frequent
+        print(res)
+        for i in range(k):
+            index = res.index(max(res))
+            ans.append(index)
+            res[index] = 0
+            
+        return ans
+	
 
+    
 
-print(sorted(array))
+    
+sol = Solution()
+sol.topKFrequent(nums = [1,1,1,2,2,3], k = 2) # [1, 2]
+
+sol.topKFrequent(nums = [1], k = 1) # [1]
