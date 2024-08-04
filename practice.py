0723b502d4f5dd1cf3e034f9353fb861392ee6ed
vsulli <sullivanvalerie5@gmail.com>
2024-08-04 16:54:36 -0500

    need to use dictionary

--- practice.py
+++ practice.py
@@ -3,18 +3,26 @@
 
 class Solution:
     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
-        ans = []
-        res = [0] * (max(nums)+1)
-        for n in set(nums):
-            count = nums.count(n)
-            res[n] = count
-		
-        # need to return indices for k most frequent
-        for i in range(k):
-            index = res.index(max(res))
-            ans.append(index)
-            res[index] = 0
-        return ans
+        count = {}
+        frequency = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            # get count for numbers, 0 default
+            count[n] = 1 + count.get(n, 0)
+
+        for n, c in count.items():
+        # add number to list at index of its frequency
+            frequency[c].append(n)
+        
+        res = []
+        # iterate list in descending order
+        for i in range(len(frequency) - 1, 0, -1):
+            # iterate through every number at that index
+            for n in frequency[i]:
+                res.append(n)
+                # return res when it's length of k
+                if len(res) == k:
+                    return res
 	
 
     
