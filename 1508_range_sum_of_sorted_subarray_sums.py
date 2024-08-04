526780780699552419b576bec83a0eaffe49fe1d
vsulli <sullivanvalerie5@gmail.com>
2024-08-04 07:31:32 -0500

    gets subarray sums

--- 1508_range_sum_of_sorted_subarray_sums.py
+++ 1508_range_sum_of_sorted_subarray_sums.py
@@ -20,12 +20,13 @@ modulo 109 + 7.
 
 class Solution:
     def rangeSum(self, nums: list[int], n: int, left: int, right: int) -> int:
+        MOD = 10 ** 9 + 7
         subarray_sums = []
-        for i in range(0, len(nums)):
-            subarray_sums.append(nums[i])
-            # need to change indices here to get all to right
-            for j in range(i, len(nums)-1):
-                subarray_sums.append(nums[j]+nums[j+1])
+        for i in range(n):
+            cur_sum = 0
+            for j in range(i, n):
+                cur_sum = (cur_sum + nums[j] % MOD)
+                subarray_sums.append(cur_sum)
 
         print(sorted(subarray_sums))
 
--- practice.py
+++ practice.py
@@ -1,4 +1,11 @@
-i = 0
-while i < 20:
-	i+=1
-	print('Multiplying with {}'.format(i))
+nums = [1,2,3,4]
+
+array = []
+j = 0
+for i in range(0, len(nums)):
+	array.append(sum(nums[:i+1]))
+	for j in range(i, len(nums)):
+		array.append(sum(nums[:j+1]))
+
+
+print(sorted(array))
