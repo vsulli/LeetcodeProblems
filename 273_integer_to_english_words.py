594b96e690e472a5952fd10b00da8b6221beae06
vsulli <sullivanvalerie5@gmail.com>
2024-08-07 18:57:40 -0500

    one way of adding to res

--- 273_integer_to_english_words.py
+++ 273_integer_to_english_words.py
@@ -11,57 +11,67 @@ num to its English words representation.
 class Solution:
 
     def numberToWords(self, num: int) -> str:
-        res = ""
-        # max number 2147483648
-        # dictionary with terms for Billion, Million, Thousand, Hundred
-        # ninety, eighty, seventy...
-        # nine, eight...
-        num_word_dict = {1000000000: 'Billion', 1000000: 'Million', 1000000: 'Hundred Thousand', 10000: 'Ten Thousand', 1000: 'Thousand', 100: 'Hundred',
-                         90: 'Ninety', 80: 'Eighty', 70: 'Seventy', 60: 'Sixty', 50: 'Fifty', 40: 'Forty', 30: 'Thirty', 20: 'Twenty',
-                         19: 'Nineteen', 18: 'Eighteen', 17: 'Seventeen', 16: 'Sixteen', 15: 'Fifteen', 14: 'Fourteen', 13: 'Thirteen',
-                         12: 'Twelve', 11: 'Eleven', 10: 'Ten', 9: 'Nine', 8: 'Eight', 7: 'Seven', 6: 'Six', 5: 'Five',
-                         4: 'Four', 3: 'Three', 2: 'Two', 1: 'One', 0: 'Zero'}
+        # base case of 0
+        if num == 0:
+            return "Zero"
         
-        # get total number of digits to know if starting with Billions, Millions, etc?
-        digits_len = len(str(num))
+        # two hashmaps
+        # 1-19
+        ones_map = {
+            1: "One",
+            2: "Two",
+            3: 'Three', 
+            4: 'Four', 
+            5: 'Five', 
+            6: 'Six', 
+            7: 'Seven', 
+            8: 'Eight', 
+            9: 'Nine',
+            10: 'Ten', 
+            11: 'Eleven',
+            12: 'Twelve',
+            13: 'Thirteen',
+            14: 'Fourteen',
+            15: 'Fifteen',
+            16: 'Sixteen',
+            17: 'Seventeen',
+            18: 'Eighteen',
+            19: 'Nineteen',
+        }
 
-        # need function to get all digits from the number and return an array?
-        digits = []
-        for digit in str(num):
-            digits.append(digit)
+        tens_map = {
+            20: "Twenty",
+            30: "Thirty",
+            40: "Forty",
+            50: "Fifty",
+            60: "Sixty",
+            70: "Seventy",
+            80: "Eighty", 
+            90: "Ninety",
+        }
 
-        match digits_len:
-            # One Digit - return result from dictionary
-            case 1:
-                return num_word_dict[num]
-            
-            # 10 -99 2
-            case 2:
-                if 10 <= num <= 20:
-                    return num_word_dict[num]
-                # need to get left-most number for hundred (1-9) + Hundred
-                # get 10s (21-99)
-                tens = int(str(digits[0]) + "0")
-                res += num_word_dict[tens]
-                res += " " + num_word_dict[int(digits[1])]
+        # converts digits into a string
+        def get_string(n):
+            return
+        
+        postfix = ["", " Thousand", " Million", " Billion"] # include space
+        i = 0 # tells you what your postfix will be 
+        res = []
 
-                return res
-            # Hundreds 3
-            case 3:
-                pass
-            case 4: 
-                pass
-            # Hundred Thousands 5
-            case 5: 
-                pass
-            # Millions will have 6
-            case 6:
-                pass
-            # Billions will have 9 digits to right
-            case 9: 
-                pass
+        while num:
+            # get last 3 digits
+            digits = num % 1000
+            s = get_string(digits) + postfix[i]
+            if s:
+               res = [s] + res 
+            # remove the digits from num by dividing
+            num //= 1000
 
-    
+        return " ".join(res)
+
+
+
+
 sol = Solution()
 
 print(sol.numberToWords(37))
--- practice.py
+++ practice.py
@@ -1,34 +1,4 @@
-# Review
-# 347 - Top K Frequent Elements
+num = 111222
 
-class Solution:
-    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
-        count = {}
-        frequency = [[] for i in range(len(nums) + 1)]
-
-        for n in nums:
-            # get count for numbers, 0 default
-            count[n] = 1 + count.get(n, 0)
-
-        for n, c in count.items():
-        # add number to list at index of its frequency
-            frequency[c].append(n)
-        
-        res = []
-        # iterate list in descending order
-        for i in range(len(frequency) - 1, 0, -1):
-            # iterate through every number at that index
-            for n in frequency[i]:
-                res.append(n)
-                # return res when it's length of k
-                if len(res) == k:
-                    return res
-	
-
-    
-
-    
-sol = Solution()
-print(sol.topKFrequent(nums = [1,1,1,2,2,3], k = 2)) # [1, 2]
-
-print(sol.topKFrequent(nums = [1], k = 1)) # [1]
+num //= 1000
+print(num)
