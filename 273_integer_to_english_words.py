71049a9cb579cf895a85ed06221edc123807fb9f
vsulli <sullivanvalerie5@gmail.com>
2024-08-06 20:09:16 -0500

    creating dictionary with English for numbers

--- 273_integer_to_english_words.py
+++ 273_integer_to_english_words.py
@@ -10,7 +10,15 @@ num to its English words representation.
 
 class Solution:
     def numberToWords(self, num: int) -> str:
-        pass
+        # max number 2147483648
+        # dictionary with terms for Billion, Million, Thousand, Hundred
+        # ninety, eighty, seventy...
+        # nine, eight...
+        num_word_dict = {1000000000: 'Billion', 1000000: 'Million', 1000: 'Thousand', 100: 'Hundred',
+                         90: 'Ninety', 80: 'Eighty', 70: 'Seventy', 60: 'Sixty', 50: 'Fifty', 40: 'Forty', 30: 'Thirty', 20: 'Twenty',
+                         19: 'Nineteen', 18: 'Eighteen', 17: 'Seventeen', 16: 'Sixteen', 15: 'Fifteen', 14: 'Fourteen', 13: 'Thirteen',
+                         12: 'Twelve', 11: 'Eleven', 10: 'Ten', 9: 'Nine', 8: 'Eight', 7: 'Seven', 6: 'Six', 5: 'Five',
+                         4: 'Four', 3: 'Three', 2: 'Two', 1: 'One'}
 
 sol = Solution()
 print(sol.numberToWords(num = 123)) 
