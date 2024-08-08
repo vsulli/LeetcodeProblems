e46b983167b2b10d767d79f0055a4c9e264c8fd3
vsulli <sullivanvalerie5@gmail.com>
2024-08-08 16:54:15 -0500

    initial commit - new problem

---
+++ 2615_sum_of_distances.py
@@ -0,0 +1,27 @@
+''' 
+Sum of Distances
+Leetcode # 2615
+vsulli
+8 August 2024
+
+You are given a 0-indexed integer 
+array nums. There exists an array 
+arr of length nums.length, where 
+arr[i] is the sum of |i - j| over 
+all j such that nums[j] == nums[i] 
+and j != i. If there is no such j, 
+set arr[i] to be 0.
+
+Return the array arr.
+ 
+'''
+class Solution:
+    def distance(self, nums: list[int]) -> list[int]:
+        pass
+
+
+sol = Solution()
+
+print(sol.distance(nums = [1,3,1,1,2])) # [5, 0, 3, 4, 0]
+
+print(sol.distance(nums = [0,5,3])) # [0, 0, 0]
