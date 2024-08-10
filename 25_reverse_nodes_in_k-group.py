134ab288feece858eda7a872cfd0f4797b89acab
vsulli <sullivanvalerie5@gmail.com>
2024-08-10 10:49:34 -0500

    neetcode solution

--- 25_reverse_nodes_in_k-group.py
+++ 25_reverse_nodes_in_k-group.py
@@ -26,36 +26,52 @@ class ListNode:
         self.next = next
         
 class Solution:
+
+    def printList(self, head):
+        while head:
+            print(head.val)
+            head = head.next
+
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         # need to have a dummy node to start result
         # set up a curr.next
         # loop through linked list reversing all nodes until count == k
         # set curr.next equal to the last node in this reversed section
         # set this reversed section as the new curr
-        dummy = ListNode(-1)
-        dummy_p = dummy
+        dummy = ListNode(0, head)
+        groupPrev = dummy # one node before reversed group
 
-        curr = head
-        count = 1
-        prev = None
-        while curr:
-            # if the count == k then assign res next to this curr node
-            if count == k:
-                pass
+        while True:
+            kth = self.getKth(groupPrev, k)
+            # if kth doesn't exist - last group not enough to reverse by k
+            if not kth:
+                break
+                
+            groupNext = kth.next
 
-            # else reverse current node and increase count
-            else:
-                count += 1
-                next_node = curr.next # holds next values
+            # reverse group
+            # 2 pointers
+            prev , curr = kth.next, groupPrev.next
+            while curr != groupNext:
+                tmp = curr.next
                 curr.next = prev
                 prev = curr
-                curr = next_node
+                curr = tmp
 
-            print(curr.val)
+            tmp = groupPrev.next
+            groupPrev.next = kth
+            groupPrev = tmp
+        return dummy.next 
+
+
+    def getKth(self, curr, k):
+        while curr and k > 0:
             curr = curr.next
+            k -= 1
 
-        return dummy.next
-            
+        return curr
+
+        
 
 sol = Solution()
 
@@ -71,5 +87,7 @@ n3.next = n4
 n4.next = n5
 
 
-print(sol.reverseKGroup(head = n1, k = 2)) # [2, 1, 4, 3, 5]
+res =sol.reverseKGroup(head = n1, k = 2) # [2, 1, 4, 3, 5]
 # reversing two at a time
+
+sol.printList(res)
