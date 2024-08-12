ab72302a4634fafcaf4b9e365b71c58f18a65e77
vsulli <sullivanvalerie5@gmail.com>
2024-08-12 17:15:05 -0500

    need to review 1 - two sum to write in < 10 mins

--- practice.py
+++ practice.py
@@ -1,23 +1,24 @@
 # 12 August 2024
 
-# Review - 217 - Contains Duplicate
+# Review - 1 - two sum
 
 from typing import List
 
 
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        seen = set()
-        for num in nums:
-            if num in seen:
-                return True
-            seen.add(num)
-        return False
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        visited = {}
+        for i in range(len(nums)):
+            if target - nums[i] in visited:
+                return [i, visited[target - nums[i]]] # difficult line
+            else:
+                visited[nums[i]] = i # difficult line
+    
 
 
 sol = Solution()
-print(sol.containsDuplicate(nums = [1,2,3,1]))
+print(sol.twoSum(nums = [2,7,11,15], target = 9))
 
-print(sol.containsDuplicate(nums = [1,2,3,4]))
+print(sol.twoSum(nums = [3,2,4], target = 6))
 
-print(sol.containsDuplicate(nums = [1,1,1,3,3,4,3,2,4,2]))
+print(sol.twoSum(nums = [3,3], target = 6))
