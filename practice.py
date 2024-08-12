c9d21ca66aca50b100e69ff549e964ce253b3db8
vsulli <sullivanvalerie5@gmail.com>
2024-08-12 16:59:35 -0500

    Reviewed again - 217 - contains duplicate - do not need i for loop or else statement. more efficient

--- practice.py
+++ practice.py
@@ -1,15 +1,23 @@
 # 12 August 2024
 
-# Review - 1929 - Concatenation of Array
+# Review - 217 - Contains Duplicate
 
 from typing import List
 
 
 class Solution:
-    def getConcatenation(self, nums: List[int]) -> List[int]:
-        return nums * 2
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
+
 
 sol = Solution()
+print(sol.containsDuplicate(nums = [1,2,3,1]))
 
-print(sol.getConcatenation(nums = [1, 2, 1]))
-print(sol.getConcatenation(nums = [1, 3, 2, 1]))
+print(sol.containsDuplicate(nums = [1,2,3,4]))
+
+print(sol.containsDuplicate(nums = [1,1,1,3,3,4,3,2,4,2]))
