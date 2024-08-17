33346a0b6b7bd2657895ea0f88c73eb64bf03fda
vsulli <sullivanvalerie5@gmail.com>
2024-08-17 09:03:21 -0500

    remove function - skip over node

--- practice.py
+++ practice.py
@@ -17,6 +17,32 @@ class LRUCache:
         self.left.next = self.right
         self.right.prev = self.left
 
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+        
+    def insert(self, node):
+
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
+    
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.cap:
+            # remove and delete lru
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
