13704f5309f15e71cfcba75728b9ccbb4d37c241
vsulli <sullivanvalerie5@gmail.com>
2024-08-18 10:03:06 -0500

    linear time and space solution - neetcode

--- 264_ugly_number_II.py
+++ 264_ugly_number_II.py
@@ -11,24 +11,27 @@ Given an integer n, return the nth ugly number.
 '''
 
 
-import heapq
 
 
 class Solution:
 
     def nthUglyNumber(self, n: int) -> int:
-        minHeap = [1]
-        visit = set()
+        nums = [1]
+        i2, i3, i5 = 0, 0 , 0
 
-        factors = [2, 3, 5]
-        for i in range(n):
-            num = heapq.heappop(minHeap)
-            if i == n - 1:
-                return num
-            for f in factors:
-                if num * f not in visit:
-                    visit.add(num * f)
-                    heapq.heappush(minHeap, num * f)
+        for i in range(1, n):
+            next_num = min(
+            nums[i2] * 2, nums[i3] * 3, nums[i5] * 5
+            )
+            nums.append(next_num)
+            if next_num == nums[i2] * 2:
+                i2 += 1
+            if next_num == nums[i3] * 3:
+                i3 += 1
+            if next_num == nums[i5] * 5:
+                i5 += 1
+
+        return nums[n - 1]
                 
         
 
