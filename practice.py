eda655ce44270a832d15531de9c1886f6779c82c
vsulli <sullivanvalerie5@gmail.com>
2024-08-19 18:21:27 -0500

    need to draw out get and put

--- practice.py
+++ practice.py
@@ -3,20 +3,42 @@
 class Node:
     def __init__(self, key, value):
         self.key, self.val = key, value
-        self.prev, self.next = None
+        self.prev =  self.next = None
 
 class LRUCache:
     def __init__(self, capacity):
         self.cap = capacity
         self.cache = {}
-        
-    def get(key: int)->int:
-        pass
-    def put(key: int, value: int):
-        pass
+
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    def get(self, key: int)->int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
     
-    def insert(node):
-        pass
-    def remove(node):
-        pass
+    def put(self, key: int, value: int):
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+    
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.next, node.prev = nxt, prev
+    
+    def remove(self, node):
+        # assign prev and next to variables
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
     
