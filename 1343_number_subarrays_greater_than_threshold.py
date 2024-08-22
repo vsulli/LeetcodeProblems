c32f19efa7d5da53c447e7dc051fc7ef2aa3bd0a
vsulli <sullivanvalerie5@gmail.com>
2024-08-21 19:48:27 -0500

    partially correct

--- 1343_number_subarrays_greater_than_threshold.py
+++ 1343_number_subarrays_greater_than_threshold.py
@@ -11,12 +11,29 @@ of sub-arrays of size k and average greater
 than or equal to threshold.
 '''
 
+import statistics
+
 class Solution:
     def numOfSubarrays(self, arr: list[int], k: int, threshold: int) -> int:
-        pass
+        left = 0
+        right = k
+        count = 0
+
+        while right != len(arr):
+            average = statistics.mean(arr[left:right])
+            if average >= threshold:
+                count +=1
+            
+            left += 1
+            right += 1
+
+        return count
+
 
 sol = Solution()
 
 print(sol.numOfSubarrays(arr = [2,2,2,2,5,5,5,8], k = 3, threshold = 4)) # output: 3
 
 print(sol.numOfSubarrays(arr = [11,13,17,23,29,31,7,5,2,3], k = 3, threshold = 5)) # output: 6
+
+print(sol.numOfSubarrays(arr = [3], k = 1, threshold = 3)) # 1
