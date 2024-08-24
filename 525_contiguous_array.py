71848c4fe6c8a9ba0be1f45361082bf4415b01e9
vsulli <sullivanvalerie5@gmail.com>
2024-08-24 12:51:54 -0500

    what needs to be the condition for searching for another pair?

--- 525_contiguous_array.py
+++ 525_contiguous_array.py
@@ -13,11 +13,28 @@ with an equal number of 0 and 1.
 
 class Solution:
     def findMaxLength(self, nums: list[int]) -> int:
-        pass
+        # will need to keep count of both 1s and 0s with the increase/shrinking of sliding window
+        l, res = 0, 0
+        count = {0: 0, 1: 0} # initialize count for each num to 0
+
+        for r in range(len(nums)):
+            count[nums[r]] += 1
+            # length of sliding has to be even 
+            if (r - l + 1) % 2 == 0:
+                if count[0] == count[1]:
+                    res = max(res, r - l + 1)
+                    # if too many of either need to increase left pointer until back in balance
+                    l += 1
+
+        return res
 
 sol = Solution()
 
 print(sol.findMaxLength(nums = [0,1])) # 2
 
 print(sol.findMaxLength(nums = [0,1,0])) # 2
+
+print(sol.findMaxLength(nums = [0,1,1,1,1,0])) # 2
+
+print(sol.findMaxLength(nums = [0, 1, 0, 1])) # 4
 
--- practice.py
+++ practice.py
@@ -1,32 +1,4 @@
-# reviewing problem
+l = 0
+r = 3
 
-class Solution:
-    def minSubArrayLen(self, target: int, nums: list[int]) -> int:
-        l, total = 0, 0
-        res = float("inf")
-
-        for r in range(len(nums)):
-            # add to sliding window
-            total += nums[r]
-            while total >= target:
-                res = min(r - l + 1, res)
-                total -= nums[l]
-                l += 1
-
-
-        return 0 if res == float("inf") else res
-            
-sol = Solution()
-
-
-print(sol.minSubArrayLen(target = 8, nums = [7])) # 0
-
-print(sol.minSubArrayLen(target = 8, nums = [8])) # 1
-
-
-print(sol.minSubArrayLen(target = 7, nums = [2,3,1,2,4,3])) # 2
-
-print(sol.minSubArrayLen(target = 4, nums = [1,4,4])) # 1
-
-print(sol.minSubArrayLen(target = 11, nums = [1,1,1,1,1,1,1,1])) # 0
-
+print((r - l + 1) % 2) 
