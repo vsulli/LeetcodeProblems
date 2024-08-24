88cbb121eaf2ddd099cc22c9638568ac60a8ec6e
vsulli <sullivanvalerie5@gmail.com>
2024-08-24 12:35:33 -0500

    initial commit

---
+++ 525_contiguous_array.py
@@ -0,0 +1,23 @@
+'''
+Contiguous Array
+Leetcode # 525
+vsulli
+24 August 2024
+
+Given a binary array nums, return the 
+maximum length of a contiguous subarray 
+with an equal number of 0 and 1.
+
+'''
+
+
+class Solution:
+    def findMaxLength(self, nums: list[int]) -> int:
+        pass
+
+sol = Solution()
+
+print(sol.findMaxLength(nums = [0,1])) # 2
+
+print(sol.findMaxLength(nums = [0,1,0])) # 2
+
