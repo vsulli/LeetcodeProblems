8bdcfa15b11737c1bd7770afc9ff49f43e78a318
vsulli <sullivanvalerie5@gmail.com>
2024-08-25 08:31:08 -0500

    practice submission - comparison of new res (r - l + 1) and previous res (res)

--- practice.py
+++ practice.py
@@ -1,4 +1,25 @@
-l = 0
-r = 3
+# 209 - practice review
 
-print((r - l + 1) % 2) 
+from typing import List
+
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        l, total = 0, 0
+        res = float("inf")
+        
+        for r in range(len(nums)):
+            total += nums[r]
+            while total >= target:
+                res = min(r - l + 1, res)
+                total -= nums[l]
+                l += 1
+
+        return 0 if res == float("inf") else res
+
+sol = Solution()
+print(sol.minSubArrayLen(target = 7, nums = [2,3,1,2,4,3])) # 2
+
+print(sol.minSubArrayLen(target = 4, nums = [1,4,4])) # 1
+
+print(sol.minSubArrayLen(target = 11, nums = [1,1,1,1,1,1,1,1])) # 0
