25700461f78cda308fa54630515fd8fc28dee542
vsulli <sullivanvalerie5@gmail.com>
2024-08-26 19:27:33 -0500

    neetcode solution using ord

--- 567_permutation_in_string.py
+++ 567_permutation_in_string.py
@@ -14,28 +14,41 @@ In other words, return true if
  the substring of s2.
 '''
 
-from collections import Counter
-
         
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        # slide through s2 O(n) going by length of s1
-        # check if count of each character matches count of s1
-        isSubstring = False
+        #edge case
+        # s1 greater than s2
+        if len(s1) > len(s2): return False
+
+        s1Count, s2Count = [0] * 26, [0] * 26
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
+
+        matches = 0
+        for i in range(26):
+            matches += (1 if s1Count[i] == s2Count[i] else 0)
+
         l = 0
-        count = {}
-        for c in s1:
-            if c in count:
-                count[c] += 1
-            else:
-                count[c] = 1
+        for r in range(len(s1), len(s2)):
+            if matches == 26: return True
+            index = ord(s2[r]) - ord('a')
+            s2Count[index] += 1
+            if s1Count[index] ==  s2Count[index]:
+                matches += 1
+            elif s1Count[index] + 1 == s2Count[index]:
+                matches -= 1
 
-        counter = Counter(s2)
-        for r in range(len(s1)-1, len(s2), len(s1)):
-            for char in count:
-                if counter[char] == count[char]
 
-        return isSubstring
+            index = ord(s2[l]) - ord('a')
+            s2Count[index] -= 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] - 1 == s2Count[index]:
+                matches -= 1
+            l += 1
+        return matches == 26
 
 sol = Solution()
 
