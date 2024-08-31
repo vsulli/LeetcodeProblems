a6c022785b03b7b119b5731a54b4c5c5ceac54d7
vsulli <sullivanvalerie5@gmail.com>
2024-08-31 17:30:27 -0500

    would need to change dictionary assignments

--- 1514_path_with_max_probability.py
+++ 1514_path_with_max_probability.py
@@ -26,11 +26,34 @@ from typing import List
 
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
-        pass
+        max_prob_path = []
+        prob = 0
+        # need to build dictionary? prob_dict # [connected nodes] : prob
+        # path_dict node: [connected]
+        prob_dict = {}
+        path_dict = {}
+
+        for i, edge_pair in enumerate(edges):
+            # add pair: prob
+            prob_dict[edge_pair[i][0]] = succProb[i]
+            # add node connections
+            if path_dict[edge_pair[i][0]] in path_dict:
+                path_dict[edge_pair[i][0]].append(edge_pair[1])
+            else:
+                 path_dict[edge_pair[i][0]] = [(edge_pair[1])]
+
+        print(prob_dict)
+        print(path_dict)
+    
+
+
+
+        return 0 if len(max_prob_path) == 0 else prob
+
 
 sol = Solution()
-print(sol.maxProbability(n = 3, edges = [[0,1],[1,2],[0,2]], succProb = [0.5,0.5,0.2], start = 0, end = 2)) 
+print(sol.maxProbability(n = 3, edges = [[0,1],[1,2],[0,2]], succProb = [0.5,0.5,0.2], start_node = 0, end_node = 2)) 
 # 0.25000
 
-print(sol.maxProbability(n = 3, edges = [[0,1],[1,2],[0,2]], succProb = [0.5,0.5,0.3], start = 0, end = 2))
+print(sol.maxProbability(n = 3, edges = [[0,1],[1,2],[0,2]], succProb = [0.5,0.5,0.3], start_node = 0, end_node = 2))
 # 0.30000
