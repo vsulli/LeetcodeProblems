b040a5942e0fe09684534f2c442317536f81ce09
vsulli <sullivanvalerie5@gmail.com>
2024-09-01 11:37:23 -0500

    practice submission

--- practice.py
+++ practice.py
@@ -6,14 +6,12 @@ from typing import List
 class Solution:
     def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
         count = 0
-        currSum = sum(arr[: k - 1])
-        
-        for l in range(len(arr) - k + 1):
-            currSum += arr[l + k - 1] # adds next index to right
-            # l as starting point, add in length of subarray, subtract 1 because indices are 0-indexed in python
-            if (currSum / k) >= threshold:
+        currSum = sum(arr[:k - 1])
+        for L in range(len(arr) - k + 1):
+            currSum += arr[L + k -1]
+            if (currSum / k ) >= threshold:
                 count += 1
-            currSum -= arr[l] # subtracts off leftmost index
+            currSum -= arr[L]
         return count
 
 
