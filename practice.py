6706ab4b7507d1bbd7cef889398ef1f2fd33c0c6
vsulli <sullivanvalerie5@gmail.com>
2024-09-01 11:27:02 -0500

    currSum += arr[l + k - 1] what does this line do? adds 3rd number?

--- practice.py
+++ practice.py
@@ -7,7 +7,13 @@ class Solution:
     def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
         count = 0
         currSum = sum(arr[: k - 1])
+        
         for l in range(len(arr) - k + 1):
+            currSum += arr[l + k - 1] # what does this line do?
+            if (currSum / k) >= threshold:
+                count += 1
+            currSum -= arr[l]
+        return count
 
 
 sol = Solution()
