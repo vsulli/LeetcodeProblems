72889003edd783c4876165ee09c73117bc4e6257
vsulli <sullivanvalerie5@gmail.com>
2024-09-02 07:53:09 -0500

    practice submission #1343

--- practice.py
+++ practice.py
@@ -6,9 +6,9 @@ from typing import List
 class Solution:
     def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
         count = 0
-        currSum = sum(arr[:k - 1])
+        currSum = sum(arr[: k - 1])
         for L in range(len(arr) - k + 1):
-            currSum += arr[L + k -1]
+            currSum += arr[L + k - 1]
             if (currSum / k ) >= threshold:
                 count += 1
             currSum -= arr[L]
