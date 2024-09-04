e7749e402adf7af47c11563c9536a056f9603f4e
vsulli <sullivanvalerie5@gmail.com>
2024-09-03 19:19:03 -0500

    one algorithm method for sliding window, maximum sum subarray size k

--- practice.py
+++ practice.py
@@ -1,30 +1,27 @@
-# leetcode 1343 practice
-
+# sliding window technique video lesson
+# https://www.youtube.com/watch?v=dOonV4byDEg&ab_channel=ProfoundAcademy
 from typing import List
 
 
 class Solution:
-    def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
-        count = 0
+    def maxofSubarray(self, arr: List[int], k: int) -> int:
+        # given an array of nums
+        # calculate the maximum of a subarray given size of window k 
         currSum = sum(arr[:k - 1])
+        currMax = currSum
 
-        for L in range(len(arr) - k + 1): # 0 - 5 are values it reaches since 6 is ending
-            print("CurrSum " + str(currSum))
-            currSum += arr[L + k - 1] # given L of 5, last index will be added here (5+ 3 - 1) = 7
-            print("L " + str(L))
-            print("CurrSum " + str(currSum))
-            print("----------")
-            if (currSum / k) >= threshold:
-                count += 1
+        for L in range(len(arr) - k + 1):
+            currSum += arr[L + k - 1]
+            if currSum > currMax:
+                currMax = currSum
             currSum -= arr[L]
-        return count
+
+        return currMax
 
 
 sol = Solution()
 
-print(sol.numOfSubarrays(arr = [2,2,2,2,5,5,5,8], k = 3, threshold = 4)) # 3
+print(sol.maxofSubarray(arr = [8, 3, -2, 4, 5, -1, 0, 5, 3, 9, -6], k = 5)) # 18, 
 
-
-
-print(sol.numOfSubarrays(arr = [11,13,17,23,29,31,7,5,2,3], k = 3, threshold = 5)) # 6
+print(sol.maxofSubarray(arr = [11,13,17,23,29,31,7,5,2,3], k = 5)) 
 
