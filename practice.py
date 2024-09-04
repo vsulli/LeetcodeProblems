448890f6f20c37ec6c1a852caa82d08fe9673381
vsulli <sullivanvalerie5@gmail.com>
2024-09-03 19:06:00 -0500

    notes for how loop and currSum indices are calculated

--- practice.py
+++ practice.py
@@ -8,8 +8,12 @@ class Solution:
         count = 0
         currSum = sum(arr[:k - 1])
 
-        for L in range(len(arr) - k + 1):
-            currSum += arr[L + k - 1]
+        for L in range(len(arr) - k + 1): # 0 - 5 are values it reaches since 6 is ending
+            print("CurrSum " + str(currSum))
+            currSum += arr[L + k - 1] # given L of 5, last index will be added here (5+ 3 - 1) = 7
+            print("L " + str(L))
+            print("CurrSum " + str(currSum))
+            print("----------")
             if (currSum / k) >= threshold:
                 count += 1
             currSum -= arr[L]
@@ -19,6 +23,8 @@ class Solution:
 sol = Solution()
 
 print(sol.numOfSubarrays(arr = [2,2,2,2,5,5,5,8], k = 3, threshold = 4)) # 3
+
+
 
 print(sol.numOfSubarrays(arr = [11,13,17,23,29,31,7,5,2,3], k = 3, threshold = 5)) # 6
 
