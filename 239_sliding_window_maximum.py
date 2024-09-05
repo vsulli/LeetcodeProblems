20d6b3cd162c3c8b0342ab9958bd6199692f1ad4
vsulli <sullivanvalerie5@gmail.com>
2024-09-05 17:59:10 -0500

    initial commit

---
+++ 239_sliding_window_maximum.py
@@ -0,0 +1,29 @@
+'''
+Sliding Window Maximum
+Leetcode # 239
+vsulli
+5 September 2024
+
+You are given an array of integers nums, 
+there is a sliding window of size k 
+which is moving from the very left 
+of the array to the very right. 
+You can only see the k numbers in 
+the window. Each time the sliding 
+window moves right by one position.
+
+Return the max sliding window.
+'''
+
+from typing import List
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        pass
+
+sol = Solution()
+
+print(sol.maxSlidingWindow(nums = [1,3,-1,-3,5,3,6,7], k = 3))
+
+print(sol.maxSlidingWindow(nums = [1], k = 1))
