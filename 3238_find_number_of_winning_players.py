31086eab4f195522623241cf22f7ecebaa4ee24d
vsulli <sullivanvalerie5@gmail.com>
2024-09-07 09:30:43 -0500

    initial commit

---
+++ 3238_find_number_of_winning_players.py
@@ -0,0 +1,39 @@
+'''
+Find the Number of Winning Players
+Leetcode # 3238
+vsulli
+7 September 2024
+
+You are given an integer n representing the 
+number of players in a game and a 2D array 
+pick where pick[i] = [xi, yi] represents that 
+the player xi picked a ball of color yi.
+
+Player i wins the game if they pick strictly 
+more than i balls of the same color. In other words,
+
+Player 0 wins if they pick any ball.
+Player 1 wins if they pick at least two balls 
+of the same color.
+...
+Player i wins if they pick at leasti + 1 balls 
+of the same color.
+Return the number of players who win the game.
+
+Note that multiple players can win the game.
+'''
+
+from typing import List
+
+
+class Solution:
+    def winningPlayerCount(self, n: int, pick: List[List[int]]) -> int:
+        pass
+
+sol = Solution()
+
+print(sol.winningPlayerCount(n = 4, pick = [[0,0],[1,0],[1,0],[2,1],[2,1],[2,0]])) # players 0 & 1 win, 2 & 3 do not
+
+print(sol.winningPlayerCount(n = 5, pick = [[1,1],[1,2],[1,3],[1,4]])) # no one wins game
+
+print(sol.winningPlayerCount(n = 5, pick = [[1,1],[2,4],[2,4],[2,4]])) # player 2 wins game by picking 3 balls with color 4
