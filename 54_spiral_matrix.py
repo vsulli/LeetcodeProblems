d3c17883f18671baca451ebe45b8d5aa7d588cce
vsulli <60162352+vsulli@users.noreply.github.com>
2024-09-09 17:43:24 -0500

    how to adjust column and rows as they get smaller?

--- 54_spiral_matrix.py
+++ 54_spiral_matrix.py
@@ -30,19 +30,27 @@ class Solution:
         count = len(res)
 
         state = 'column_down'
+        offset_row = 1
+        offset_column = 0
+        row = 1
+        column = len(matrix)
 
-        '''
-        while count < total_len:
+        
+        for i in range(len(matrix)):
             # switch based on state?
             if state == 'row_right':
                 state = 'column_down'
+
             if state == 'column_down':
+                for j in range(column):
+                    print(matrix[j][column - offset_column])
+
                 state = 'row_left'
             if state == 'row_left':
                 state = 'column_up'
             if state == 'column_up':
                 state = 'row_right'
-            '''
+
 
         return res
 
@@ -52,7 +60,9 @@ class Solution:
 
 sol = Solution()
 
+'''
 print(sol.spiralOrder(matrix = [[1,2,3],[4,5,6],[7,8,9]]))
 
+'''
 
 print(sol.spiralOrder(matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
