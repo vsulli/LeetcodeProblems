a6b3590eac77a3ed8f64880dbf93921766d0604e
vsulli <60162352+vsulli@users.noreply.github.com>
2024-09-09 17:15:04 -0500

    prints all values of matrix

--- 54_spiral_matrix.py
+++ 54_spiral_matrix.py
@@ -14,10 +14,23 @@ from typing import List
 
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        pass
+        # return the elements fo the matrix in spiral order
+        # values from -100 to 100
+        # rows/columns from 1 to 10 max
+        
+        # order will be entire first row, last column, last row
+        # then first column up to 2nd row to 2nd to last column
+
+
+        res = []
+        for row in range(len(matrix[0])):
+            for column in range(len(matrix[1])):
+                print(matrix[row][column])
 
 sol = Solution()
 
 print(sol.spiralOrder(matrix = [[1,2,3],[4,5,6],[7,8,9]]))
+'''
 
 print(sol.spiralOrder(matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
+'''
