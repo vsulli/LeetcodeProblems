93d20d8e7ce9c1c86b0a983158a3991284815478
vsulli <60162352+vsulli@users.noreply.github.com>
2024-09-09 17:01:57 -0500

    initial commit

---
+++ 54_spiral_matrix.py
@@ -0,0 +1,23 @@
+
+'''
+Spiral Matrix
+Leetcode # 54
+vsulli
+9 September 2024
+
+Given an m x n matrix, return all 
+elements of the matrix in spiral order.
+'''
+
+from typing import List
+
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        pass
+
+sol = Solution()
+
+print(sol.spiralOrder(matrix = [[1,2,3],[4,5,6],[7,8,9]]))
+
+print(sol.spiralOrder(matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
