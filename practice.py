1a28f816cc54d5621d5f4f59e661f5f96586a493
vsulli <60162352+vsulli@users.noreply.github.com>
2024-09-11 18:47:29 -0500

    practice submission

--- practice.py
+++ practice.py
@@ -6,10 +6,28 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        pass
+        # left and right pointer
+        # initialize middle 
+        # while l < r
+        # if m > target, set right == middle
+        # if m < target, set left == middle
+        # if it equals target, return it
+        # otherwise return -1
+
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                return m
+        return -1
 
 sol = Solution()
 
-print(sol.search(nums = [-1,0,3,5,9,12], target = 9))
+print(sol.search(nums = [-1,0,3,5,9,12], target = 9)) # 4
 
-print(sol.search(nums = [-1,0,3,5,9,12], target = 2))
+print(sol.search(nums = [-1,0,3,5,9,12], target = 2)) # -1
