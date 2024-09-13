67145af35ad72184d5a9edc89dde30fe0bfb217c
vsulli <60162352+vsulli@users.noreply.github.com>
2024-09-13 10:43:54 -0500

    need to add binary search

--- 74_search_2d_matrix.py
+++ 74_search_2d_matrix.py
@@ -21,7 +21,38 @@ from typing import List
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        pass
+        # need to select middle of middle array?
+        # need to maintain 2 pointers to beginning and end of that row of matrix?
+        # if m isn't in that range then set to lower or higher row?
+        # it is possible for no answer to be found
+
+        contains_target = False
+        # assign to middle matrix
+        l, r = 0,len(matrix[0]) - 1
+
+        ml, mr = 0, len(matrix) - 1
+        mid_row = (ml + mr) // 2
+        while l <= r:
+            mid_row = (ml + mr) // 2
+            # if target is in that row
+            if  matrix[mid_row][l] <= target <= matrix[mid_row][r]:
+                print("in row")
+                return
+            elif target < matrix[mid_row][l]:
+                mr = mid_row - 1
+            elif target > matrix[mid_row][r]:
+                ml = mid_row + 1
+                
+        
+
+
+        # if in range of that row search by regular binary search
+
+        # if smaller, assign to smaller matrix
+
+        # if larger, assign to larger matrix
+        return contains_target
+
 
 sol = Solution()
 
