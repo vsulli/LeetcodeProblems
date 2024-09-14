3c2b4bbbbde3f630d9e5f82f01b4d818100f3394
vsulli <60162352+vsulli@users.noreply.github.com>
2024-09-14 08:13:03 -0500

    does not take into account lexicographical/alphabetical order

--- 692_top_k_frequent_words.py
+++ 692_top_k_frequent_words.py
@@ -1,3 +1,4 @@
+# TODO learn how to sort a hashmap in descending order
 '''
 Top K Frequent Words
 Leetcode # 692
@@ -19,9 +20,27 @@ from typing import List
 
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        pass
+        # loop through words
+        # if a word hasn't been seen, add to hashmap
+        # if it already has, increment its value
+        # then sort the hashmap and return highest ones for k
+        res = []
+        freq = {}
+        for w in words:
+            if w in freq:
+                freq[w] += 1
+            else:
+                freq[w] = 1
+        
+        sorted_freq = dict(sorted(freq.items(), key=lambda item: item[1], reverse=True))
+        # sorted_keys = sorted_freq.keys()
+        res = list(sorted_freq.keys())[:k]
+
+        return res
 
 sol = Solution()
+
+print(sol.topKFrequent(words = ['i'], k = 1))
 
 print(sol.topKFrequent(words = ["i","love","leetcode","i","love","coding"], k = 2))
 
