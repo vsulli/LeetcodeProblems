c7c1b7dc089c29e8eb08889175fa8d594ec5d9dd
vsulli <60162352+vsulli@users.noreply.github.com>
2024-09-15 10:31:45 -0500

    prints out each pixel

--- 3030_find_the_grid_region_average.py
+++ 3030_find_the_grid_region_average.py
@@ -1,18 +1,32 @@
 '''
-Search a 2D Matrix
-Leetcode # 74
+Find the Grid of Region Average
+Leetcode # 3030
 vsulli
-13 September 2024
+15 September 2024
 
-You are given an m x n integer matrix with the following properties:
+You are given m x n grid image which represents a grayscale 
+image, where image[i][j] represents a pixel with intensity 
+in the range [0..255]. You are also given a non-negative 
+integer threshold.
 
-Each row is sorted in non-decreasing order.
-The first integer of each row is greater than 
-the last integer of the previous row.
-Given an integer target, return true if target 
-is in matrix or false otherwise.
+Two pixels are adjacent if they share an edge.
 
-You must write a solution in O(log(m * n)) time complexity.
+A region is a 3 x 3 subgrid where the absolute difference 
+in intensity between any two adjacent pixels is less than 
+or equal to threshold.
+
+All pixels in a region belong to that region, note that a 
+pixel can belong to multiple regions.
+
+You need to calculate a m x n grid result, where result[i][j] 
+is the average intensity of the regions to which image[i][j] 
+belongs, rounded down to the nearest integer. If image[i][j] 
+belongs to multiple regions, result[i][j] is the average of the 
+rounded-down average intensities of these regions, rounded down 
+to the nearest integer. If image[i][j] does not belong to any 
+region, result[i][j] is equal to image[i][j].
+
+Return the grid result.
 '''
 
 from typing import List
@@ -20,11 +34,22 @@ from typing import List
 
 class Solution:
     def resultGrid(self, image: List[List[int]], threshold: int) -> List[List[int]]:
-        pass
+        # how to determine if a pixel is adjacent?
+        # region is 3x3 
+
+        for row in range(len(image)):
+            for column in range(len(image[0])):
+                print(image[row][column])
+
 
 
 sol = Solution()
 
 print(sol.resultGrid(image = [[5,6,7,10],[8,9,10,10],[11,12,13,10]], threshold = 3))
+# Output: [[9,9,9,9],[9,9,9,9],[9,9,9,9]]
 
 print(sol.resultGrid(image = [[10,20,30],[15,25,35],[20,30,40],[25,35,45]], threshold = 12))
+# Output: [[25,25,25],[27,27,27],[27,27,27],[30,30,30]]
+
+print(sol.resultGrid([[5,6,7],[8,9,10],[11,12,13]], threshold = 1))
+# Output: [[5,6,7],[8,9,10],[11,12,13]]
