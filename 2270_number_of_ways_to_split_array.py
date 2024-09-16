ed4bd43a948e22f5781b9b9b6c15e89271b90906
vsulli <60162352+vsulli@users.noreply.github.com>
2024-09-16 16:29:33 -0500

    initial commit

---
+++ 2270_number_of_ways_to_split_array.py
@@ -0,0 +1,28 @@
+'''
+Number of Ways to Split Array
+Leetcode # 2270
+vsulli
+16 September 2024
+
+Given an array of strings words and 
+an integer k, return the k most 
+frequent strings.
+
+Return the answer sorted by the 
+frequency from highest to lowest. 
+Sort the words with the same frequency 
+by their lexicographical order.
+'''
+
+from typing import List
+
+
+class Solution:
+    def waysToSplitArray(self, nums: List[int]) -> int:
+        pass
+
+sol = Solution()
+
+print(sol.waysToSplitArray(nums = [10,4,-8,7]))
+
+print(sol.waysToSplitArray(nums = [2,3,1,0]))
