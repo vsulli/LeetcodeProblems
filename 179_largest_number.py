df8f55ec4f5a691811f134cfd9a1971b9ec1d2c8
vsulli <60162352+vsulli@users.noreply.github.com>
2024-09-18 16:38:36 -0500

    initial commit

---
+++ 179_largest_number.py
@@ -0,0 +1,30 @@
+'''
+Largest Number
+Leetcode # 179
+vsulli
+18 September 2024
+
+Given a list of non-negative integers 
+nums, arrange them such that they form 
+the largest number and return it.
+
+Since the result may be very large, 
+so you need to return a string instead 
+of an integer.
+
+'''
+
+from typing import List
+
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        pass
+
+sol = Solution()
+
+print(sol.largestNumber(nums = [10,2]))
+
+print(sol.largestNumber(nums = [3,30,34,5,9]))
+
+
