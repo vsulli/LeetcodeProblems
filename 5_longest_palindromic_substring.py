fa6da38e711fa65d106abcbf7285fb6d1d69c9fc
vsulli <60162352+vsulli@users.noreply.github.com>
2024-09-19 18:46:44 -0500

    how to calculate if still a palindrome?

--- 5_longest_palindromic_substring.py
+++ 5_longest_palindromic_substring.py
@@ -10,10 +10,19 @@ Given a string s, return the longest palindromic substring in s.
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        pass
+        # two pointers
+        # start one pointer at end of string
+        res = ''
+        p = len(s) - 1
+
+        for i in range(len(s)):
+            res += s[i]
+            # check if it's a palindrome by getting from middle index to end(reverse order) and beg to middle index
+            
+        
 
 sol = Solution()
 
-print(sol.longestPalindrome(s = "babad"))
+print(sol.longestPalindrome(s = "babad")) # bab or aba
 
-print(sol.longestPalindrome(s = "cbbd"))
+print(sol.longestPalindrome(s = "cbbd")) # bb
