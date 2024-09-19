9904de3521ee663ed832722f1e00a1c0c2601c6a
vsulli <60162352+vsulli@users.noreply.github.com>
2024-09-19 18:36:50 -0500

    initial commit

---
+++ 5_longest_palindromic_substring.py
@@ -0,0 +1,19 @@
+'''
+Longest Palindromic Substring
+Leetcode # 5
+vsulli
+19 September 2024
+
+Given a string s, return the longest palindromic substring in s.
+
+'''
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        pass
+
+sol = Solution()
+
+print(sol.longestPalindrome(s = "babad"))
+
+print(sol.longestPalindrome(s = "cbbd"))
