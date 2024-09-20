98aaa0249a631e99b8b6bebab29fdc96c7402980
vsulli <60162352+vsulli@users.noreply.github.com>
2024-09-20 09:28:37 -0500

    neetcode solution: need to consider each character the center and while possible to expand out do that

--- 5_longest_palindromic_substring.py
+++ 5_longest_palindromic_substring.py
@@ -10,39 +10,34 @@ Given a string s, return the longest palindromic substring in s.
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # two pointers
-        # start one pointer at end of string
-        res = ''
-        m = len(s) // 2
-        p = len(s) - 1
-        if s[m::-1] == s[m:p+1]:
-            res = s[m:p+1]
-        p-=1
+        res = ""
+
+        resLen = 0
 
         for i in range(len(s)):
-            # print(s[m:i:-1])
-            # print(s[m:p+1])
-            # slice string in neg direction
-            if s[m:i:-1] == s[m:p+1]:
-                print(s[m:i:-1])
-                print(s[m:p+1])
-                print("---------")
-                if len(s) // 2 == 0:
-                    new_res = s[m:p+1] # if even length string
-                elif len(s) // 2 != 0:
-                    new_res = s[m-1:p+1] # if odd length string
-                if len(new_res) > len(res):
-                    res = new_res
-                
-            # check if it's a palindrome by getting from middle index to end(reverse order) and beg to middle index
-            p-=1
+            # odd length 
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l:r+1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+
+            # even length
+            l, r = i, i + 1
+            while l >= 0 and r <len(s) and s[l] == s[r]:
+                if (r - l + 1) > resLen:
+                    res = s[l:r+1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+
         return res
 
 sol = Solution()
 
-'''
 
 print(sol.longestPalindrome(s = "babad")) # bab or aba
-'''
 
 print(sol.longestPalindrome(s = "cbbd")) # bb
