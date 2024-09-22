229d4316025375d25ff45fb14796b893b0be331a
vsulli <60162352+vsulli@users.noreply.github.com>
2024-09-22 08:33:05 -0500

    initial commit

---
+++ 516_longest_palindromic_subsequence.py
@@ -0,0 +1,25 @@
+'''
+Longest Palindromic Subsequence
+Leetcode # 516
+vsulli
+22 September 2024
+
+Given a string s, find the longest palindromic 
+subsequence's length in s.
+
+A subsequence is a sequence that can be 
+derived from another sequence by deleting 
+some or no elements without changing the order 
+of the remaining elements.
+
+'''
+
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        pass
+
+sol = Solution()
+
+print(sol.longestPalindromeSubseq(s = "bbbab"))
+
+print(sol.longestPalindromeSubseq(s = "cbbd"))
