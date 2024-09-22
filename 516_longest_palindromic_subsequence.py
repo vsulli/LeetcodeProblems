005cc0f92c56cf799cb6ce10850c93319f974f4e
vsulli <60162352+vsulli@users.noreply.github.com>
2024-09-22 08:38:24 -0500

    before adapting it to delete some elements

--- 516_longest_palindromic_subsequence.py
+++ 516_longest_palindromic_subsequence.py
@@ -16,7 +16,35 @@ of the remaining elements.
 
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        pass
+        # similar to # 5
+        # make a copy of s
+        # when checking if next letter matches, if it doesn't, 
+        # delete that character and check next one until it does match or you reach end of string?
+
+        res = ""
+        resLen = 0
+
+        for i in range(len(s)):
+
+            # odd number s
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if r - l + 1 > resLen:
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+
+            # even number s
+            l, r = i, i+1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if r - l + 1 > resLen:
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+            
+
+
+        return resLen
 
 sol = Solution()
 
