fd66e1ae80590bb58306e557639f115f32c60974
vsulli <60162352+vsulli@users.noreply.github.com>
2024-09-27 16:50:01 -0500

    binary tree solution

--- 729_my_calendar_I.py
+++ 729_my_calendar_I.py
@@ -26,20 +26,42 @@ without causing a double booking. Otherwise, return
 false and do not add the event to the calendar.
 '''
 
+class Tree:
+    def __init__(self, start, end):
+        self.left = None
+        self.right = None
+        self.start = start
+        self.end = end
+
+    def insert(self, start, end):
+        curr = self
+        while True:
+            if start >= curr.end:
+                if not curr.right:
+                    curr.right = Tree(start, end)
+                    return True
+                curr = curr.right
+            elif end <= curr.start:
+                if not curr.left:
+                    curr.left = Tree(start, end)
+                    return True
+                curr = curr.left
+            else:
+                return False
+
+
 class MyCalendar:
 
     def __init__(self):
-        self.schedule = []
+        self.root = None
+        # binary search tree
 
     def book(self, start: int, end: int) -> bool:
-        # scan through list of events
-        for s, e in self.schedule:
-            # overlap return false
-            if e > start and end > s:
-                return False
-        # don't overlap, add to schedule
-        self.schedule.append((start, end))
-        return True
+        if not self.root:
+            self.root = Tree(start, end)
+            return True
+        return self.root.insert(start, end)
+
         
 
 
