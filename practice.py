df8a7f04fc0bd27844c2061f9c53ce8752122a16
vsulli <60162352+vsulli@users.noreply.github.com>
2024-09-29 09:55:08 -0500

    practice submission - leetcode #729

--- practice.py
+++ practice.py
@@ -5,20 +5,16 @@
 class MyCalendar:
 
     def __init__(self):
-        self.schedule = [] 
+        self.schedule = []
 
     def book(self, start: int, end: int) -> bool:
-        # if end of new one to book is between start and end, return False
-        # if beginning is between start and end, return False
-        for i in range(len(self.schedule)):
-            # valid if new start is greater than or equal to sched end
-            # valid if new end is less than or equal to sched start
-            if not(start >= self.schedule[i][1] or end <= self.schedule[i][0]):
+        for s, e in self.schedule:
+            if not (end <= s or e <= start):
                 return False
-        
-        # valid, so add 
+
         self.schedule.append([start, end])
         return True
+
 
 
 
