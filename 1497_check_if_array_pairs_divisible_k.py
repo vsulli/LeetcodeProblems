b7e1b8a716c3920cae16432e168921988565665b
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-01 13:28:36 -0500

    get frequency of remainders and match up

--- 1497_check_if_array_pairs_divisible_k.py
+++ 1497_check_if_array_pairs_divisible_k.py
@@ -19,7 +19,26 @@ from typing import List
 
 class Solution:
     def canArrange(self, arr: List[int], k: int) -> bool:
-        pass
+        # match up the remainders to the count of numbers with that remainder
+        freq = [0] * k
+        
+        for num in arr:
+            remainder = (num % k + k) % k
+            freq[remainder] += 1
+        
+        if freq[0] % 2 != 0:  
+            return False
+        
+        for i in range(1, k // 2 + 1):
+            if freq[i] != freq[k - i]:  
+                return False
+        
+        return True
 
 sol = Solution()
 
+print(sol.canArrange(arr = [1,2,3,4,5,10,6,7,8,9], k = 5)) # True
+
+print(sol.canArrange(arr = [1,2,3,4,5,6], k = 7)) # True
+
+print(sol.canArrange(arr = [1,2,3,4,5,6], k = 10)) # False
