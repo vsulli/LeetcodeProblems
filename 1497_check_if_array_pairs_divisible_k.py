493b6ac6fefcb4177502388886898328171328eb
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-01 13:09:46 -0500

    initial commit

---
+++ 1497_check_if_array_pairs_divisible_k.py
@@ -0,0 +1,25 @@
+'''
+My Calendar I
+Leetcode # 1497
+vsulli
+1 October 2024
+
+Given an array of integers arr of even length 
+n and an integer k.
+
+We want to divide the array into exactly n / 2 
+pairs such that the sum of each pair is divisible by k.
+
+Return true If you can find a way to do that or 
+false otherwise.
+'''
+
+from typing import List
+
+
+class Solution:
+    def canArrange(self, arr: List[int], k: int) -> bool:
+        pass
+
+sol = Solution()
+
