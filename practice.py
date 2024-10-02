e5b67169497d7d56d276f0ff8d3f07965a67517a
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-02 10:07:41 -0500

    not quite right. doesn't work for second case where k  = 7

--- practice.py
+++ practice.py
@@ -6,7 +6,27 @@ from typing import List
 
 class Solution:
     def canArrange(self, arr: List[int], k: int) -> bool:
-        pass
+        # need to loop through array finding what the remainder is for each number
+        # store that in a dictionary rem: count
+        # make sure that for each key, equal count
+        # how to handle negative numbers?
+        divisible = True
+
+        freq_rem = {}
+
+        for i in range(len(arr)):
+            rem = arr[i] % 5
+            if rem in freq_rem:
+                freq_rem[rem] += 1
+            else:
+                freq_rem[rem] = 1
+
+        # print(freq_rem)
+        
+        for key in freq_rem:
+            if not(freq_rem.get(key) % 2 == 0):
+                return False
+        return divisible
 
 sol = Solution()
 
