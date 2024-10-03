bf318e541dce53b18f04f5c7cc249af4f7585d15
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-03 08:19:47 -0500

    prefix sum solution with dictionary

--- 1590_make_sum_divisible_by_p.py
+++ 1590_make_sum_divisible_by_p.py
@@ -17,6 +17,8 @@ the array.
  
 '''
 
+from collections import defaultdict
+from itertools import accumulate
 from typing import List
 
 
@@ -27,16 +29,26 @@ class Solution:
         # the remainder is what you need to get rid of in order to make it divisible by p
         # if remainder is 0 then total array sum is divisible by p and you can return 0
         # [0, 3, 4, 8, 10]  8 - 4 = 4, length 1 - min subarray to remove
-        
 
-        
+        prefix_sum = list(accumulate(nums, initial = 0))
+        m = prefix_sum[-1] % p
+        if m == 0:
+            return 0
+        d = defaultdict(int)
+        n = len(nums)
+        res = n
+        for i, v in enumerate(prefix_sum):
+            d[v % p] = i
+            if (v - m) % p in d:
+                res = min(res, i - d[(v - m) % p])
+        return res if res < n else -1
         
 
 
 sol = Solution()
 
-print(sol.minSubarray(nums = [3,1,4,2], p = 6))
+print(sol.minSubarray(nums = [3,1,4,2], p = 6)) # 1
 
-print(sol.minSubarray(nums = [6,3,5,2], p = 9))
+print(sol.minSubarray(nums = [6,3,5,2], p = 9)) # 2
 
-print(sol.minSubarray(nums = [1,2,3], p = 3))
+print(sol.minSubarray(nums = [1,2,3], p = 3)) # 0
