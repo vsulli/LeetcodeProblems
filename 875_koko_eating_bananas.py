7368c10c99764bae42cb45cb6a666f7444d23fd8
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-04 10:22:17 -0500

    would need to get rid of new pile or just move pointer once less than = 0?

--- 875_koko_eating_bananas.py
+++ 875_koko_eating_bananas.py
@@ -29,12 +29,28 @@ class Solution:
         # can only have enough time to finish all piles if you eat the max each time
         if h == len(piles):
             return max(piles)
+        time = 0
+        p = 0
+        for i in range(1, max(piles)+1):
+            n_piles = piles
+            while n_piles and n_piles[p] != 0:
+                n_piles[p] -= i 
+                if n_piles[p] <= 0:
+                    p += 1
+                if p >= len(piles):
+                    break
+            if time == h:
+                return i
+
+
+
+
         
 
 sol = Solution()
 
-print(sol.minEatingSpeed(piles = [3,6,7,11], h = 8))
+print(sol.minEatingSpeed(piles = [3,6,7,11], h = 8)) # 4
 
-print(sol.minEatingSpeed(piles = [30,11,23,4,20], h = 5))
+print(sol.minEatingSpeed(piles = [30,11,23,4,20], h = 5)) # 30
 
-print(sol.minEatingSpeed(piles = [30,11,23,4,20], h = 6))
+print(sol.minEatingSpeed(piles = [30,11,23,4,20], h = 6)) # 23
