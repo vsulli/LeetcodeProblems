ebbc57fdc2b3643d376922d34f4a99290dced756
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-04 10:30:07 -0500

    not subtracting from last column

--- 875_koko_eating_bananas.py
+++ 875_koko_eating_bananas.py
@@ -29,15 +29,21 @@ class Solution:
         # can only have enough time to finish all piles if you eat the max each time
         if h == len(piles):
             return max(piles)
+        
         time = 0
         p = 0
         for i in range(1, max(piles)+1):
+            time = 0
+            p = 0
             n_piles = piles
-            while n_piles and n_piles[p] != 0:
+            while n_piles[p] > 0:
                 n_piles[p] -= i 
+                time += 1
+                print(n_piles)
                 if n_piles[p] <= 0:
                     p += 1
-                if p >= len(piles):
+                if p >= len(piles) - 1:
+                    print("here")
                     break
             if time == h:
                 return i
@@ -51,6 +57,9 @@ sol = Solution()
 
 print(sol.minEatingSpeed(piles = [3,6,7,11], h = 8)) # 4
 
+'''
+
 print(sol.minEatingSpeed(piles = [30,11,23,4,20], h = 5)) # 30
 
 print(sol.minEatingSpeed(piles = [30,11,23,4,20], h = 6)) # 23
+'''
