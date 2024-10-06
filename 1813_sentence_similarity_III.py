56893960da1433858bf18435cb3694d85f39a8f6
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-06 08:50:14 -0500

    how to deal with need for insertions both before and after matches?

--- 1813_sentence_similarity_III.py
+++ 1813_sentence_similarity_III.py
@@ -13,20 +13,40 @@ class Solution:
     # take longer sentence as guide
     # for each index in this sentence, check if it exists in other sentence and get the indices
         isSimilar = True
+        match_indices = [] # add the indices of where the word occurs in first sentence and second sentence
 
-        
-        # convert each string to a list, splitting by space
+        sentence1 = sentence1.split()
         sentence2 = sentence2.split()
-        for w in sentence2:
-            print(w)
 
+        # first sentence longer
+        if len(sentence1) > len(sentence2):
+            for i in range(len(sentence1)):
+                if sentence1[i] in sentence2:
+                    # [sentence1 index, sentence2 index]
+                    match_indices.append([i, sentence2.index(sentence1[i])])
+
+        # second sentence longer
+        else:
+            for i in range(len(sentence2)):
+                if sentence2[i] in sentence1:
+                    match_indices.append([sentence1.index(sentence2[i]), i])
+        print(match_indices)
+        print("---------")
         return isSimilar
 
 sol = Solution()
 
 print(sol.areSentencesSimilar(sentence1 = "My name is Haley", sentence2 = "My Haley")) # true
+# "My" occurs at index 0 for both
+# "Haley" occurs at index 3 for 1st, 1 for second
+# [[0, 0], [3, 1]]
 
 print(sol.areSentencesSimilar(sentence1 = "of", sentence2 = "A lot of words")) # false
 # would have to insert before and after
+# "of" occurs at position 0 for 1st, 2 for second
+# since not the first position for 2nd sentence, know that there are matches before and after
+# [[0, 2]]
 
-print(sol.areSentencesSimilar(sentence1 = "Eating right now", sentence2 = "Eating"))
+print(sol.areSentencesSimilar(sentence1 = "Eating right now", sentence2 = "Eating")) # true
+# only one match and both in same position, can insert rest either before or after, return true
+# [[0, 0]]
