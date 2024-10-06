7b8bbc05cc1b9407d0610e07ce869c9bb16c68b0
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-06 08:33:07 -0500

    able to split string on whitespaces

--- 1813_sentence_similarity_III.py
+++ 1813_sentence_similarity_III.py
@@ -9,9 +9,18 @@ vsulli
 
 class Solution:
     def areSentencesSimilar(self, sentence1: str, sentence2: str) -> bool:
-        pass
     # as long as indices of different words is all in a row, then possible to insert?
+    # take longer sentence as guide
+    # for each index in this sentence, check if it exists in other sentence and get the indices
+        isSimilar = True
 
+        
+        # convert each string to a list, splitting by space
+        sentence2 = sentence2.split()
+        for w in sentence2:
+            print(w)
+
+        return isSimilar
 
 sol = Solution()
 
