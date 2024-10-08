dce33203d21e8f2655b253b777fbf63852d899c4
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-08 17:36:31 -0500

    need if statement for when stack is empty

--- practice.py
+++ practice.py
@@ -4,7 +4,19 @@
 
 class Solution:
     def minLength(self, s: str) -> int:
-        pass
+        # can pop 'AB' or 'CD'
+        stack = []
+        for c in s:
+            if not stack:
+                stack.append(c)
+                continue
+            if c == 'B' and stack[-1] == 'A':
+                stack.pop()
+            elif c == 'D' and stack[-1] == 'C':
+                stack.pop()
+            else:
+                stack.append(c)
+        return len(stack)
 
 
 sol = Solution()
