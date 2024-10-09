661e06358a333eff3092f919a1ff517b134d7481
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-09 18:44:24 -0500

    initial commit

---
+++ 921_minimum_add_to_make_parentheses_valid.py
@@ -0,0 +1,31 @@
+'''
+Minimum Add to Make Parentheses Valid
+Leetcode # 921
+vsulli
+9 October 2024
+
+A parentheses string is valid if and only if:
+
+It is the empty string,
+It can be written as AB (A concatenated with B), 
+where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+You are given a parentheses string s. In one move, 
+you can insert a parenthesis at any position of the string.
+
+For example, if s = "()))", you can insert an opening 
+parenthesis to be "(()))" or a closing parenthesis to be "())))".
+Return the minimum number of moves required to make s valid.
+
+'''
+
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        pass
+
+sol = Solution()
+
+print(sol.minAddToMakeValid(s = "())")) # 1
+
+print(sol.minAddToMakeValid(s = "(((")) # 3
+
