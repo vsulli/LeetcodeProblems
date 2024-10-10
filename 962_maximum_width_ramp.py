9e4de1c7b04f5790e461ee8e7dff2ef62bbcc6f5
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-10 16:32:46 -0500

    works for 2 test cases. failing [2, 2, 1]

--- 962_maximum_width_ramp.py
+++ 962_maximum_width_ramp.py
@@ -20,13 +20,25 @@ class Solution:
     def maxWidthRamp(self, nums: List[int]) -> int:
         # two pointer
         # advance second pointer as long as second num >= first
+            # may be nums in middle that are smaller...
+            # start pointer at end?
         # once no longer true, get length j - i
         # keep track of max len 
-        pass
+        max_len = 0
+        p1, p2 = 0, len(nums) - 1
+
+        while p1 != p2:
+            if nums[p2] >= nums[p1]:
+                max_len = max(max_len, p2 - p1)
+            
+            p1 += 1
+        return max_len
+            
+
 
 sol = Solution()
 
-print(sol.maxWidthRamp(nums = [6,0,8,2,1,5]))
+print(sol.maxWidthRamp(nums = [6,0,8,2,1,5])) # 4
 
-print(sol.maxWidthRamp(nums = [9,8,1,0,1,9,4,0,4,1]))
+print(sol.maxWidthRamp(nums = [9,8,1,0,1,9,4,0,4,1])) # 7
 
