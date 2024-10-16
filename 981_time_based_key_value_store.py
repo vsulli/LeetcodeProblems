420b71f30fe5220881c9dd0d8cdf98501b8408d7
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-16 17:53:02 -0500

    set operation

--- 981_time_based_key_value_store.py
+++ 981_time_based_key_value_store.py
@@ -25,11 +25,13 @@ largest timestamp_prev. If there are no values, it returns "".
 class TimeMap:
 
     def __init__(self):
-        self.time_map = {}
+        self.time_map = {}# key : list of [val, timestamp]
 
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.time_map[key, timestamp] = value
+        if key not in self.time_map:
+            self.time_map[key] = []
+        self.time_map[key].append([value, timestamp])
 
     def get(self, key: str, timestamp: int) -> str:
 
