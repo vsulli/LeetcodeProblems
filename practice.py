5b6bb35dedfcbe78f514fb347a4cc9a843a5f2ac
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-17 16:30:15 -0500

    practice submission

--- practice.py
+++ practice.py
@@ -1,16 +1,32 @@
 # 981 Time Based Key-Value Store
 
 class TimeMap:
-
     def __init__(self):
-        
+        self.store = {}
 
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-       
+        if key not in self.store:
+            self.store[key] = []
+        self.store[key].append([value, timestamp])
+           
 
     def get(self, key: str, timestamp: int) -> str:
+        res = ""
+        # retrieve list of values for that key
+        values = self.store.get(key, []) # default is empty list
+        l, r = 0, len(values) - 1
 
+        while l <= r:
+            m = (l + r) // 2
+
+            if values[m][1] <= timestamp:
+                res = values[m][0]
+                l = m + 1
+            else:
+                r = m - 1
+
+        return res
         
 
 
