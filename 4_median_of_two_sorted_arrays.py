d925e18f458a64ab8611a2e5e38ac7fab3f77645
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-18 09:25:48 -0500

    neetcode solution

--- 4_median_of_two_sorted_arrays.py
+++ 4_median_of_two_sorted_arrays.py
@@ -16,31 +16,39 @@ should be O(log (m+n)).
 '''
 
 from typing import List
+# Time: log(min(n, m))
+
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        # need to merge array first?
-        # or use pointers in both lists?
+        A, B = nums1, nums2
+        total = len(nums1) + len(nums2)
+        half = total // 2
 
-        l1, r1 = 0, 0
-        l2, r2 = len(nums1) - 1, len(nums2) - 1
-         # case where both lists are same length
-        if len(nums1) == len(nums2):
-           return (nums1[l2] + nums2[r1]) / 2
+        if len(B) < len(A):
+            A, B = B, A
 
-        total_len = len(nums1) + len(nums2)
+        l, r = 0, len(A) - 1
+        while True:
+            i = (l + r) // 2  # A
+            j = half - i - 2  # B
 
-        # case where one list is empty?
-         
-        # case where both lists are len 1
+            Aleft = A[i] if i >= 0 else float("-infinity")
+            Aright = A[i + 1] if (i + 1) < len(A) else float("infinity")
+            Bleft = B[j] if j >= 0 else float("-infinity")
+            Bright = B[j + 1] if (j + 1) < len(B) else float("infinity")
 
-        # case where len odd
-
-        # case where len even
-
-
-
-
+            # partition is correct
+            if Aleft <= Bright and Bleft <= Aright:
+                # odd
+                if total % 2:
+                    return min(Aright, Bright)
+                # even
+                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+            elif Aleft > Bright:
+                r = i - 1
+            else:
+                l = i + 1
 
 
 sol = Solution()
