82027ca588629352d3e3501e731841d18999fe23
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-19 07:50:02 -0500

    practice submission

--- practice.py
+++ practice.py
@@ -15,7 +15,20 @@ class TimeMap:
         
 
     def get(self, key: str, timestamp: int) -> str:
-       
+        res = ""
+        values = self.store.get(key, [])
+        l, r = 0, len(values) - 1
+
+        while l <= r:
+            m = (l + r) //2 
+
+            if values[m][1] <= timestamp:
+                res = values[m][0]
+                l = m + 1
+            else:
+                r = m - 1
+
+        return res
         
 
 
