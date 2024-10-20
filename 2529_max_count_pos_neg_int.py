f0b72387b0cddf1bd29960ddc00a32b71775824f
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-20 07:41:59 -0500

    initial commit

---
+++ 2529_max_count_pos_neg_int.py
@@ -0,0 +1,30 @@
+
+'''
+Maximum Count of Positive Integer and Negative Integer
+Leetcode #2529
+vsulli
+20 October 2024
+
+Given an array nums sorted in non-decreasing order, 
+return the maximum between the number of positive 
+integers and the number of negative integers.
+
+In other words, if the number of positive integers 
+in nums is pos and the number of negative integers 
+is neg, then return the maximum of pos and neg.
+Note that 0 is neither positive nor negative.
+'''
+
+from typing import List
+
+class Solution:
+    def maximumCount(self, nums: List[int]) -> int:
+        pass
+
+sol = Solution()
+
+print(sol.maximumCount(nums = [-2,-1,-1,1,2,3])) # 3
+
+print(sol.maximumCount(nums = [-3,-2,-1,0,0,1,2])) # 3
+
+print(sol.maximumCount(nums = [5,20,66,1314])) # 4
