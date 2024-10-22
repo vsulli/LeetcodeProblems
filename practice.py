aba39bc8c00182f3a8d61efc5ad44fea89cbb0f6
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-22 06:02:32 -0500

    practice submission

--- practice.py
+++ practice.py
@@ -1,46 +1,26 @@
-# 981 Time Based Key-Value Store
+# 704 Binary Search 
 
-class TimeMap:
-    def __init__(self):
-        self.store = {}
+from typing import List
 
 
-    def set(self, key: str, value: str, timestamp: int) -> None:
-        # if key doesn't exist, create new list
-        if not key in self.store:
-            self.store[key] = []
-        
-        # if key already exists, append
-        self.store[key].append([value, timestamp])
-        
-
-    def get(self, key: str, timestamp: int) -> str:
-        res = ""
-        values = self.store.get(key, [])
-        l, r = 0, len(values) - 1
-
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
         while l <= r:
-            m = (l + r) //2 
-
-            if values[m][1] <= timestamp:
-                res = values[m][0]
+            m = (l + r) // 2
+            if nums[m] > target:
+                r = m -1
+            elif nums[m] < target:
                 l = m + 1
             else:
-                r = m - 1
-
-        return res
-        
+                return m
+        return -1
 
 
+sol = Solution()
 
+print(sol.search(nums = [-1,0,3,5,9,12], target = 9))
 
-# Your TimeMap object will be instantiated and called as such:
-obj = TimeMap()
-obj.set("foo", "bar", 1)
-param_2 = obj.get("foo", 1) # bar
-print(param_2)
-param_2 = obj.get("foo", 3) # timestamp 1 -> bar
-print(param_2)
-obj.set("foo", "bar", 1)
+print(sol.search(nums = [-1,0,3,5,9,12], target = 2))
 
 
