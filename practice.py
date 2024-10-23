f353e7de71537ac9d88b9a1c7dfbc3002df74085
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-23 16:28:11 -0500

    practice submission # 74

--- practice.py
+++ practice.py
@@ -1,26 +1,50 @@
-# 704 Binary Search 
+# 74 Search a 2D Matrix
 
 from typing import List
 
 
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) - 1
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # figure out which row it needs to be in, then do binary search on that row
+        # log m + log n
+        ROWS, COLS = len(matrix), len(matrix[0])
+
+        # look for row you need
+        # two pointers
+        top, bot = 0, ROWS - 1
+        while top <= bot:
+            mid_row = (top + bot) // 2
+            if target > matrix[mid_row][-1]: # rightmost value in row
+                top = mid_row + 1 # going down creates larger values
+                # look at rows that are larger
+            elif target < matrix[mid_row][0]:
+                bot = mid_row - 1
+            else: # target is in range
+                break 
+
+        
+        if not (top <= bot):
+            return False # target does not appear in matrix
+        
+        # run binary search on middle row
+        row = (top + bot) // 2
+        l, r = 0, COLS - 1 # rightmost number in row
         while l <= r:
             m = (l + r) // 2
-            if nums[m] > target:
-                r = m -1
-            elif nums[m] < target:
-                l = m + 1
+            if target > matrix[row][m]:
+                l = m + 1 # search to right
+            elif target < matrix[row][m]:
+                r = m - 1 # search to left
             else:
-                return m
-        return -1
+                return True
+        
+        return False
+
+
 
 
 sol = Solution()
 
-print(sol.search(nums = [-1,0,3,5,9,12], target = 9))
+print(sol.searchMatrix(matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3)) # true
 
-print(sol.search(nums = [-1,0,3,5,9,12], target = 2))
-
-
+print(sol.searchMatrix(matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13)) # false
