c53e85a01d47c8960f1fa4e97274c818c5790bbb
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-24 16:37:13 -0500

    initial commit

---
+++ 701_insert_into_binary_search_tree.py
@@ -0,0 +1,20 @@
+# TODO
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        pass
+
+sol = Solution()
+
+
+root = TreeNode(val = 4, left = 2, right = 7)
+TreeNode()
