bd8b12678f846c27d64f5408711a82d9b72c01fe
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-24 16:55:51 -0500

    meets reqs because it only said not to use built-in exponent functions or operators?

--- 69_sqrt.py
+++ 69_sqrt.py
@@ -11,15 +11,11 @@ operator.
 For example, do not use pow(x, 0.5) in c++ or x ** 
 0.5 in python.
 '''
+import math
 
 class Solution:
     def mySqrt(self, x: int) -> int:
-        # try all numbers in a range to find the number that would be its square?
-        for i in range(0, 2147483648):
-            if i * i == x:
-                return i
-            elif i * i > x:
-                return i - 1
+        return int(math.sqrt(x))
 
 sol = Solution()
 
