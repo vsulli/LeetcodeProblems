01e61826e7d4bbdee0475cf95274f49356728012
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-27 06:45:15 -0500

    practice submission - recursive solution

--- practice.py
+++ practice.py
@@ -1,24 +1,27 @@
-# Definition for a binary tree node.
+# 701 Insert into a Binary Search Tree
+from typing import Optional
+
 class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
+    def __init__(self, val = 0, left = None, right = None):
         self.val = val
         self.left = left
         self.right = right
 
-from typing import Optional
+
 class Solution:
-    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+    def insertIntoBST(self, root : Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        # no root node
         if not root:
             return TreeNode(val)
 
-        # val less than root value
+        # need to go left
         if val < root.val:
             root.left = self.insertIntoBST(root.left, val)
 
-        # val greater than root value
+        # need to go right
         else:
             root.right = self.insertIntoBST(root.right, val)
-        
+            
         return root
     
 root = TreeNode(4)
