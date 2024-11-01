9dc63309a079e16b60d3d71ad021dfcde450aaab
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-31 21:12:10 -0500

    practice submission 704

--- practice.py
+++ practice.py
@@ -6,7 +6,20 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        pass
+        l, r = 0, len(nums) - 1
+        res = -1
+
+        while l <= r:
+            m = (l + r) // 2
+
+            if nums[m] < target: 
+                l = m + 1
+            elif nums[m] > target:
+                r = m - 1
+            else:
+                return m
+
+        return res if l > r else l
 
 sol = Solution()
 
