8161e0ae12f45f60a9d59f848fbbe59d067a9e04
vsulli <60162352+vsulli@users.noreply.github.com>
2024-10-31 21:07:33 -0500

    initial commit

--- practice.py
+++ practice.py
@@ -1,31 +1,15 @@
-# 701 Insert into a Binary Search Tree
-from typing import Optional
+# 704 Binary Search
 
-class TreeNode:
-    def __init__(self, val = 0, left = None, right = None):
-        self.val = val
-        self.left = left
-        self.right = right
+
+from typing import List
 
 
 class Solution:
-    def insertIntoBST(self, root : Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        if not root:
-            return TreeNode(val)
-        
-        if val < root.val:
-            root.left = self.insertIntoBST(root.left, val)
-        
-        else:
-            root.right = self.insertIntoBST(root.right, val)
-        return root
-    
-root = TreeNode(4)
+    def search(self, nums: List[int], target: int) -> int:
+        pass
 
 sol = Solution()
 
-sol.insertIntoBST(root, 2)
-sol.insertIntoBST(root, 7)
-sol.insertIntoBST(root, 1)
-sol.insertIntoBST(root, 3)
-sol.insertIntoBST(root, 5)
+print(sol.search(nums = [-1,0,3,5,9,12], target = 9))
+
+print(sol.search(nums = [-1,0,3,5,9,12], target = 2))
