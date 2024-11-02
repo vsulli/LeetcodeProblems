a51e58fa8590a13fd7853f8536fce8528a9ca64a
vsulli <60162352+vsulli@users.noreply.github.com>
2024-11-02 09:30:57 -0500

    #450 delete node bst practice submission

--- practice.py
+++ practice.py
@@ -11,41 +11,37 @@ from typing import Optional
 
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int)-> Optional[TreeNode]:
-        # if no root, return root
         if not root:
             return root
+        
+        # find node to delete
 
-        # navigate to where value would be
-        if key < root.val:
+        # go to right
+        if key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        
+        # go to left
+        elif key < root.val:
             root.left = self.deleteNode(root.left, key)
-        
-        elif key > root.val:
-            root.left = self.deleteNode(root.right, key)
 
-        
+        # found node to delete
         else:
+            # left of root is null?
+            # return right
             if not root.left:
                 return root.right
-            elif not root.left:
-                return root.left 
-            
-            cur = root.right 
-            while cur:
-                cur = cur.right
-            
+
+            elif not root.right:
+                return root.left
         
+            # find minimum of right subtree
+            cur = root.right
+            while cur.left: # stop at a valid node
+                cur = cur.left 
+            # by end of loop cur points at min val of right subtree
+            root.val = cur.val
+            root.right = self.deleteNode(root.right, root.val) # deleting now duplicate value of cur val/root val in right subtree
         return root
-
-
-        # if no left child
-        # if no right child
-
-        # cur pointer
-        # recursively call delete?
-
-
-        return root
-        
 
 
 
