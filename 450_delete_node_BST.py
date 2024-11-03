f79a0ff048907672b1f96f4a0fafd42dbeb6709f
vsulli <60162352+vsulli@users.noreply.github.com>
2024-11-03 08:01:40 -0600

    450 - delete node practice submission

--- 450_delete_node_BST.py
+++ 450_delete_node_BST.py
@@ -40,32 +40,23 @@ class Solution:
 
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         if not root:
-                return root
-            
-        # go left
+            return root
+        
         if key < root.val:
             root.left = self.deleteNode(root.left, key)
-
-        # go right
         elif key > root.val:
             root.right = self.deleteNode(root.right, key)
 
-        # found node to delete
         else:
-            # no left child
-            if not root.left:
-                return root.right
-            # no right child
-            elif not root.right:
+            if not root.right:
                 return root.left
+            elif not root.left:
+                return root.right
             
-            # find min of right subtree
             cur = root.right
-
             while cur.left:
                 cur = cur.left
-            
-            # set new root val
+
             root.val = cur.val
             root.right = self.deleteNode(root.right, root.val)
         return root
