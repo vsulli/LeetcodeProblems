9964bed4e01787e153c30858f4fe56604a02aa66
vsulli <60162352+vsulli@users.noreply.github.com>
2024-11-11 17:19:24 -0600

    bfs level order traversal

--- 102_binary_tree_level_order_traversal.py
+++ 102_binary_tree_level_order_traversal.py
@@ -4,10 +4,14 @@ Leetcode # 102
 vsulli
 11 November 2024
 
-
+Given the root of a binary tree, 
+return the level order traversal of 
+its nodes' values. (i.e., from left to 
+right, level by level).
 '''
 
 from typing import Optional, List
+import collections
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -17,7 +21,26 @@ class TreeNode:
 class Solution:
 
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        pass
+        # bfs 
+        res = []
+
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            qLen = len(q)
+            level = []
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level)
+
+        return res
+
 
 sol = Solution()
 
