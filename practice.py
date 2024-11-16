753f7d64baf7266d5a1ed70c8748d0f36f756f97
vsulli <60162352+vsulli@users.noreply.github.com>
2024-11-16 12:35:25 -0600

    leetcode #450 practice submission - delete node bst

--- practice.py
+++ practice.py
@@ -1,4 +1,4 @@
-# 102 Binary Tree Level Order Traversal
+# 450 delete node in a bst
 
 # Definition for a binary tree node.
 
@@ -20,11 +20,41 @@ class Solution:
             root.left = self.insertIntoBST(root.left, val)
         return root
 
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return root
+        
+        # navigate to where the node would be
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+
+        else:
+            # if not left child
+            if not root.right:
+                return root.left
+            # if not right child
+            elif not root.left:
+                return root.right
+            # go to minimum of right subtree (which will be leftmost)
+            curr = root.right
+            while curr.left:
+                curr = curr.left
+            # have to set new value and delete recursively
+            root.val = curr.val
+            root.right = self.deleteNode(root.right, root.val)
+
+        return root
+
+
 sol = Solution()
 
-root = TreeNode(4)
-sol.insertIntoBST(root, 2)
-sol.insertIntoBST(root, 7)
-sol.insertIntoBST(root, 1)
+root = TreeNode(5)
 sol.insertIntoBST(root, 3)
-print(sol.insertIntoBST(root, val = 5))
+sol.insertIntoBST(root, 6)
+sol.insertIntoBST(root, 2)
+sol.insertIntoBST(root, 4)
+sol.insertIntoBST(root, 7)
+
+print(sol.deleteNode(root, 3))
