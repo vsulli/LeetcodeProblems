291f9c331e6763d6352bb459c98742e28a93cf09
vsulli <60162352+vsulli@users.noreply.github.com>
2024-11-19 17:46:32 -0600

    delete node practice submission

--- practice.py
+++ practice.py
@@ -19,11 +19,38 @@ class Solution:
 
         if val > root.val:
             root.right = self.insertIntoBST(root.right, val)
+
+        return root
+    
+
+    def deleteNode(self, root: Optional[TreeNode], key: int)-> Optional[TreeNode]:
+        if not root:
+            return root
+        
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+
+        else:
+            if not root.left:
+                return root.right
+            if not root.right:
+                return root.left
+            
+            curr = root.right
+            while curr.left:
+                curr = curr.left
+            root.val = curr.val
+            root.right = self.deleteNode(root.right, root.val)
             
         return root
+
 
 sol = Solution()
 
 root = TreeNode(4)
 
 sol.insertIntoBST(root, 5)
+sol.deleteNode(root, 4)
