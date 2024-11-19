4fb62e95f7f300d34441d0342dc12b02d8ce0f25
vsulli <60162352+vsulli@users.noreply.github.com>
2024-11-18 18:36:47 -0600

    #242 - valid anagram - return sorted equals

--- practice.py
+++ practice.py
@@ -1,66 +1,11 @@
-# 144 binary tree preorder traversal
-
-# Definition for a binary tree node.
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.right = right
-        self.left = left
-
-from typing import Optional, List
-import collections
+# 242 - Valid Anagram
 
 class Solution:
-    def insertIntoBST(self, root:Optional[TreeNode], value: int)->Optional[TreeNode]:
-        if not root:
-            return TreeNode(value)
-        if value < root.val:
-            root.left = self.insertIntoBST(root.left, value)
-        elif value > root.val:
-            root.right = self.insertIntoBST(root.right, value)
-        return root
-    def deleteNode(self, root:Optional[TreeNode], key:int)->Optional[TreeNode]:
-        if not root:
-            return root 
-
-        # navigate to area of key
-        if key < root.val:
-            root.left = self.deleteNode(root.left, key)
-        elif key > root.val:
-            root.right = self.deleteNode(root.right, key)
-        else:
-            # if no left child
-            if not root.left:
-                return root.right
-
-            # if no right child
-            if not root.right:
-                return root.left
-
-            # on right subtree, find min
-            curr = root.right
-            while curr.left:
-                curr = curr.left 
-            root.val = curr.val
-            root.right = self.deleteNode(root.right, root.val)
-        
-        return root
-
-
-    
-        
-            
-            
-            
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
 
 sol = Solution()
 
-root = TreeNode(5)
-sol.insertIntoBST(root, 3)
-sol.insertIntoBST(root, 6)
-sol.insertIntoBST(root, 2)
-sol.insertIntoBST(root, 4)
-sol.insertIntoBST(root, 7)
+print(sol.isAnagram(s = "anagram", t = "nagaram"))
 
-print(sol.deleteNode(root, 3))
+print(sol.isAnagram(s = "rat", t = "car"))
