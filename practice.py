d906d7c946e72c3df5cd50523740fdea47d3f1b3
vsulli <60162352+vsulli@users.noreply.github.com>
2024-11-18 18:33:44 -0600

    delete node practice submission

--- practice.py
+++ practice.py
@@ -22,48 +22,33 @@ class Solution:
         return root
     def deleteNode(self, root:Optional[TreeNode], key:int)->Optional[TreeNode]:
         if not root:
-            return root
-        # navigate to where node to delete is 
+            return root 
+
+        # navigate to area of key
         if key < root.val:
             root.left = self.deleteNode(root.left, key)
         elif key > root.val:
             root.right = self.deleteNode(root.right, key)
         else:
-            # if not left or right child
-            if not root.right:
-                return root.left
+            # if no left child
             if not root.left:
                 return root.right
-            
-            # go to min of right subtree (leftmost)
+
+            # if no right child
+            if not root.right:
+                return root.left
+
+            # on right subtree, find min
             curr = root.right
             while curr.left:
-                curr = curr.left
+                curr = curr.left 
             root.val = curr.val
             root.right = self.deleteNode(root.right, root.val)
-
+        
         return root
 
 
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        res = []
-        q = collections.deque()
-
-        if root:
-            q.append(root)
-        
-        while q:
-            level = []
-            for _ in range(len(q)):
-                node = q.popleft()
-                level.append(node.val)
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-            if level:
-                res.append(level)
-        return res
+    
         
             
             
@@ -79,5 +64,3 @@ sol.insertIntoBST(root, 4)
 sol.insertIntoBST(root, 7)
 
 print(sol.deleteNode(root, 3))
-
-print(sol.levelOrder(root))
