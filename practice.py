f4b3cc2df11f0bd1ef4280cd1ab51ef267820520
vsulli <60162352+vsulli@users.noreply.github.com>
2024-11-18 18:41:30 -0600

    not a faster runtime

--- practice.py
+++ practice.py
@@ -2,7 +2,26 @@
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        # count of each character must be the same and they must be the same length
+        if len(s) != len(t):
+            return False
+        
+        isAnagram = True
+        count_s = {}
+        count_t = {}
+
+        for i in range(len(s)):
+            if s[i] in count_s:
+                count_s[s[i]] += 1
+            if s[i] not in count_s:
+                count_s[s[i]] = 1
+            if t[i] in count_t:
+                count_t[t[i]] += 1
+            if t[i] not in count_t:
+                count_t[t[i]] = 1
+        
+
+        return count_s == count_t
 
 sol = Solution()
 
