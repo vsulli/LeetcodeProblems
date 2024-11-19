466fe83a1fa2dfda79b3eda4680b9fe09d2de456
vsulli <60162352+vsulli@users.noreply.github.com>
2024-11-18 18:46:31 -0600

    Neetcode - optimal hashtable solution

--- practice.py
+++ practice.py
@@ -2,26 +2,18 @@
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # count of each character must be the same and they must be the same length
         if len(s) != len(t):
             return False
-        
-        isAnagram = True
-        count_s = {}
-        count_t = {}
 
+        count = [0] * 26
         for i in range(len(s)):
-            if s[i] in count_s:
-                count_s[s[i]] += 1
-            if s[i] not in count_s:
-                count_s[s[i]] = 1
-            if t[i] in count_t:
-                count_t[t[i]] += 1
-            if t[i] not in count_t:
-                count_t[t[i]] = 1
-        
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
 
-        return count_s == count_t
+        for val in count:
+            if val != 0:
+                return False
+        return True
 
 sol = Solution()
 
