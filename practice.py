bc7eb2e60e03a4221f998ee460587fe0fdd708a2
vsulli <60162352+vsulli@users.noreply.github.com>
2024-11-19 17:30:40 -0600

    leetcode #242 practice submission #2

--- practice.py
+++ practice.py
@@ -4,11 +4,13 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
+        
         d = [0] * 26
+
         for i in range(len(s)):
             d[ord(s[i]) - ord('a')] += 1
             d[ord(t[i]) - ord('a')] -= 1
-
+        
         for val in d:
             if val != 0:
                 return False
