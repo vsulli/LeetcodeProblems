a17b7befd20b2f72d0c375f14d1f80cc86a1ac08
vsulli <60162352+vsulli@users.noreply.github.com>
2024-11-19 18:02:19 -0600

    leetcode #1 two sum - practice submission

--- practice.py
+++ practice.py
@@ -1,53 +1,16 @@
-# 701 - Insert into a BST
-
-from typing import Optional
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
+# 1 Two Sum
+from typing import List
 
 class Solution:
-    def insertIntoBST(self, root: Optional[TreeNode], val:int)->Optional[TreeNode]:
-        if not root:
-            return TreeNode(val)
+    def twoSum(self, nums: List[int], target:int)->List[int]:
+        seen = {}
+        for i in range(len(nums)):
+            if target - nums[i] in seen:
+                return [seen[target - nums[i]], i]
+            else:
+                seen[nums[i]] = i
         
-        if val < root.val:
-            root.left = self.insertIntoBST(root.left, val)
-
-        if val > root.val:
-            root.right = self.insertIntoBST(root.right, val)
-
-        return root
-    
-
-    def deleteNode(self, root: Optional[TreeNode], key: int)-> Optional[TreeNode]:
-        if not root:
-            return root
-        
-        if key < root.val:
-            root.left = self.deleteNode(root.left, key)
-        elif key > root.val:
-            root.right = self.deleteNode(root.right, key)
-        else:
-            if not root.left:
-                return root.right
-            elif not root.right:
-                return root.left
-            
-            curr = root.right
-            while curr.left:
-                curr = curr.left
-            root.val = curr.val
-            root.right = self.deleteNode(root.right, root.val)
-        return root
-
 
 sol = Solution()
 
-root = TreeNode(4)
-
-sol.insertIntoBST(root, 5)
-sol.deleteNode(root, 4)
+print(sol.twoSum(nums = [2, 7, 11, 15], target = 9))
