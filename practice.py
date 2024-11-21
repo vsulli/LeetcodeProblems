26a1233bf320de0f1dbbbb7538a0e0dc272aa265
vsulli <60162352+vsulli@users.noreply.github.com>
2024-11-20 19:27:17 -0600

    leetcode #347 practice submission - have to use lambda

--- practice.py
+++ practice.py
@@ -4,17 +4,20 @@ from typing import List
 class Solution:
     def topKFrequent(self, nums:List[int], k:int)->List[int]:
         res = []
+        count = {}
 
-        d = {}
         for n in nums:
-            if n in d:
-                d[n] += 1
+            if n in count:
+                count[n] += 1
             else:
-                d[n] = 1
+                count[n] = 1
         
-        for i, v in enumerate(d):
-            res.append(v)
-            if i == k-1:
+        # have to return top k, so sort dictionary by key, then append to res in range k
+        count = sorted(count.items(), key=lambda item: item[1], reverse=True)
+
+        for i,t in enumerate(count):
+            res.append(t[0])
+            if i == k - 1:
                 return res
 
 
@@ -23,3 +26,5 @@ sol = Solution()
 print(sol.topKFrequent(nums = [1,1,1,2,2,3], k = 2)) # [1, 2]
 
 print(sol.topKFrequent(nums = [1], k = 1)) # [1]
+
+print(sol.topKFrequent(nums = [1,1,2,2,3,3,3,3,3,4,4,4], k = 2)) # [3,4]
