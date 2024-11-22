bb19e36a81626acc0457afe6ad43f319a26fa99d
vsulli <60162352+vsulli@users.noreply.github.com>
2024-11-22 16:17:06 -0600

    leetcode #1 two sum practice submission

--- practice.py
+++ practice.py
@@ -1,37 +1,20 @@
-# 450 delete node in a BST
-from typing import Optional
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# 1 Two Sum
+
+from typing import List
 
 class Solution:
-    def deleteNode(self, root: Optional[TreeNode],key: int)->Optional[TreeNode]:
-        if not root:
-            return root
-        
-        # if key < root 
-        if key < root.val:
-            root.left = self.deleteNode(root.left, key)
+    def twoSum(self, nums: List[int], target: int)->List[int]:
+        res = []
+        seen = {} # num: index
+        for i, n in enumerate(nums):
+            if target - n in seen:
+                return [seen[target - n], i]
+            else:
+                seen[n] = i
+        return res
 
-        # if key > root
-        elif key > root.val:
-            root.right = self.deleteNode(root.right, key)
 
-        else:
-            # if no left child
-            # if no right child
-            if not root.left:
-                return root.right
-            elif not root.right:
-                return root.left
-    
-            # find min of right subtree
-            curr = root.right
-            while curr.left:
-                curr = curr.left
-            root.val = curr.val
-            root.right = self.deleteNode(root.right, root.val)
-        return root
+sol = Solution()
 
+print(sol.twoSum(nums = [2,7,11,15], target = 9)) # [0, 1]
+print(sol.twoSum(nums = [3,2,4], target = 6))
