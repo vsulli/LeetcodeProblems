054a24f05aedc4974d6632a650f5cd0a5eaa1e6f
vsulli <60162352+vsulli@users.noreply.github.com>
2024-11-23 11:18:33 -0600

    leetcode #242 valid anagram practice submission #1

--- practice.py
+++ practice.py
@@ -1,22 +1,10 @@
-# 49 group anagrams
+# 242 valid anagram
 
-from typing import List
 class Solution:
-    def groupAnagrams(self, strs: List[str])->List[List[str]]:
-        res = []
-        d = {}
-
-        for w in strs:
-            if tuple(sorted(w)) in d:
-                d[tuple(sorted(w))].append(w)
-            else:
-                d[tuple(sorted(w))] = [w]
-        
-        for l in d.values():
-            res.append(l)
-        return res
-
+    def isValid(self, s: str, t: str)->bool:
+        return sorted(s) == sorted(t)
 sol = Solution()
 
-print(sol.groupAnagrams(strs = ["eat","tea","tan","ate","nat","bat"]))
-print(sol.groupAnagrams(strs = [""]))
+print(sol.isValid(s = "anagram", t = "nagaram"))
+print(sol.isValid(s = "rat", t = "car"))
+
