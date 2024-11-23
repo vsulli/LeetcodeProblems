029ba4e4c175fb617ee38ed16d1c0eb589443b18
vsulli <60162352+vsulli@users.noreply.github.com>
2024-11-23 11:32:48 -0600

    leetcode #242 valid anagram practice submission - initialize array to length of alphabet

--- practice.py
+++ practice.py
@@ -1,10 +1,23 @@
 # 242 valid anagram
 
 class Solution:
-    def isValid(self, s: str, t: str)->bool:
-        return sorted(s) == sorted(t)
+    def isAnagram(self, s: str, t: str)->bool:
+        # check if words are same length
+        if len(s) != len(t):
+            return False 
+        # initialize an array of length of alphabet
+        count = [0] * 26
+
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+        
+        for val in count:
+            if val != 0:
+                return False
+        return True
+
 sol = Solution()
 
-print(sol.isValid(s = "anagram", t = "nagaram"))
-print(sol.isValid(s = "rat", t = "car"))
-
+print(sol.isAnagram(s = "anagram", t = "nagaram"))
+print(sol.isAnagram(s = "rat", t = "car"))
