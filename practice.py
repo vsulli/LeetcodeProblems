1bf04bd2a1946bb7a797658edfe82bf163722322
vsulli <60162352+vsulli@users.noreply.github.com>
2024-11-24 08:58:40 -0600

    leetcode #242 valid anagram practice submission

--- practice.py
+++ practice.py
@@ -1,52 +1,26 @@
-# 701 Insert into a Binary Search Tree
-# 450 Delete ndoe in a BST
-
-from typing import Optional
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# 242 Valid Anagram
 
 class Solution:
-    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]: 
-        if not root:
-            return TreeNode(val)
-        elif val < root.val:
-            root.left = self.insertIntoBST(root.left, val)
-        else:
-            root.right = self.insertIntoBST(root.right, val)
-        return root
-    
-    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        if not root:
-            return root
+    def isAnagram(self, s: str, t: str)->bool:
+        # check if same length]
+        if len(s) != len(t):
+            return False
+        # create an array for each letter
+        count = [0] * 26
+        for i in range(len(s)):
+            # increase and decrease that letter index for s and t
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
         
-        elif key < root.val:
-            root.left = self.deleteNode(root.left, key)
-        elif key > root.val:
-            root.right = self.deleteNode(root.right, key)
+        for i in range(len(count)):
+            if count[i] != 0:
+                return False
+        return True
+    
 
-        else:
-            if not root.left:
-                return root.right
-            elif not root.right:
-                return root.left
-            else:
-                curr = root.right
-                while curr.left:
-                    curr = curr.left
-                root.val = curr.val
-                root.right = self.deleteNode(root.right, root.val)
-        return root
 
-root = TreeNode(4)
+        
+
 sol = Solution()
-
-sol.insertIntoBST(root, 2)
-sol.insertIntoBST(root, 7)
-sol.insertIntoBST(root, 1)
-sol.insertIntoBST(root, 3)
-sol.insertIntoBST(root, 5)
-
-sol.deleteNode(root, 1)
+print(sol.isAnagram(s = "anagram", t = "nagaram"))
+print(sol.isAnagram("rat", t = "car"))
