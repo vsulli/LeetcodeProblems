58e9ac65dc9d6f2f8236802a9a11650f1eae68c3
vsulli <60162352+vsulli@users.noreply.github.com>
2024-11-24 09:17:34 -0600

    leetcode #271 encode and decode strings - separate with special character

--- practice.py
+++ practice.py
@@ -1,26 +1,36 @@
-# 242 Valid Anagram
+# 271 Encode and Decode Strings
+# Neetcode
+
+from typing import List
 
 class Solution:
-    def isAnagram(self, s: str, t: str)->bool:
-        # check if same length]
-        if len(s) != len(t):
-            return False
-        # create an array for each letter
-        count = [0] * 26
-        for i in range(len(s)):
-            # increase and decrease that letter index for s and t
-            count[ord(s[i]) - ord('a')] += 1
-            count[ord(t[i]) - ord('a')] -= 1
-        
-        for i in range(len(count)):
-            if count[i] != 0:
-                return False
-        return True
-    
+    def encode(self, strs: List[str])-> str:
+        res = ''
+        # separate each word on the ">" character
+        for w in strs:
+            res += w + ">"
+        return res
 
-
-        
+    def decode(self, s: str)-> List[str]:
+        res = []
+        w = ""
+        # increment pointer through word, when you reach special symbol, append word to res
+        p = 0
+        while p != (len(s)):
+            if s[p] == ">":
+                res.append(w)
+                w = ""
+            else:
+                w += s[p]
+            p += 1
+        return res
 
 sol = Solution()
-print(sol.isAnagram(s = "anagram", t = "nagaram"))
-print(sol.isAnagram("rat", t = "car"))
+
+encoded = sol.encode(["neet","code","love","you"])
+print(encoded)
+print(sol.decode(encoded))
+
+encoded2 = sol.encode(["we","say",":","yes"])
+print(encoded2)
+print(sol.decode(encoded2))
