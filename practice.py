70b1bfed64506ad41a8d69bf76ef678512b3019a
vsulli <60162352+vsulli@users.noreply.github.com>
2024-11-23 19:15:39 -0600

    leetcode # 450 delete node in a bst - need to use if, elif, else in third section

--- practice.py
+++ practice.py
@@ -1,4 +1,5 @@
 # 701 Insert into a Binary Search Tree
+# 450 Delete ndoe in a BST
 
 from typing import Optional
 class TreeNode:
@@ -16,7 +17,37 @@ class Solution:
         else:
             root.right = self.insertIntoBST(root.right, val)
         return root
+    
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        # if not node
+        if not root:
+            return root
+
+        # go left 
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+
+        # go right
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
         
+        else:
+
+            # no left child
+            if not root.left:
+                return root.right
+            # no right child
+            if not root.right:
+                return root.left
+
+            # find min of right subtree
+            curr = root.right
+            while curr.left:
+                curr = curr.left
+            root.val = curr.val
+            root.right = self.deleteNode(root.right, root.val)
+
+        return root
 
 root = TreeNode(4)
 sol = Solution()
@@ -26,3 +57,5 @@ sol.insertIntoBST(root, 7)
 sol.insertIntoBST(root, 1)
 sol.insertIntoBST(root, 3)
 sol.insertIntoBST(root, 5)
+
+sol.deleteNode(root, 1)
