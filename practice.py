f07f2d24e41369a1268daf8a642625868de6e8d0
vsulli <60162352+vsulli@users.noreply.github.com>
2024-11-30 15:02:44 -0600

    leetcode #242 valid anagram

--- practice.py
+++ practice.py
@@ -1,23 +1,22 @@
-#347 top k frequent elements
-from typing import List
+#242 Valid Anagram
 
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        res = []
-        counts = {} # num: count
-        for n in nums:
-            if n in counts:
-                counts[n] += 1
-            else:
-                counts[n] = 1
-        sorted_counts = sorted(counts.items(), key=lambda items:items[1], reverse=True)
-        for i,n in enumerate(sorted_counts):
-            res.append(n[0])
-            if i == k - 1:
-                return res
-
-
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+       
+        count = [0] * 26
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+        
+        for v in count:
+            if v != 0:
+                return False
+        
+        return True
 
 sol = Solution()
 
-print(sol.topKFrequent(nums = [1,1,1,2,2,3], k = 2))
+print(sol.isAnagram(s = "anagram", t = "nagaram"))
+print(sol.isAnagram(s = "car", t = "tar"))
