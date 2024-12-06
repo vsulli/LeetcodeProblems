2e92db76eb5e5b7f5ef330dd3721acd433b22e2b
vsulli <60162352+vsulli@users.noreply.github.com>
2024-12-06 12:36:01 -0600

    off by 1?

--- 2554_max_num_integers_range_I.py
+++ 2554_max_num_integers_range_I.py
@@ -28,12 +28,26 @@ from typing import List
 
 class Solution:
     def maxCount(self, banned: List[int], n: int, maxSum: int) -> int:
-        pass
+        max = 0
+        nums = set()
+        for i in range(1, n+1):
+            if i not in banned:
+                nums.add(i)
+            elif i in banned and i == maxSum:
+                return max
+            curr_sum = sum(nums)
+            if curr_sum > maxSum:
+                return max
+            else:
+                max += 1
+
 
 sol = Solution()
 
 print(sol.maxCount(banned = [1,6,5], n = 5, maxSum = 6))
 
+'''
 print(sol.maxCount(banned = [1,2,3,4,5,6,7], n = 8, maxSum = 1))
 
 print(sol.maxCount(banned = [11], n = 7, maxSum = 50))
+'''
