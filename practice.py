d33cc58701120d78913bc1bed11d85400fa3eccf
vsulli <60162352+vsulli@users.noreply.github.com>
2024-12-07 09:23:03 -0600

    adapted solution

--- practice.py
+++ practice.py
@@ -1,4 +1,26 @@
-n = 0
-for c in "_L__R__R_":
-    print(str(n) + " " + c)
-    n += 1
+# leetcode #2554
+
+from typing import List
+
+class Solution:
+    def maxCount(self, banned: List[int], n: int, maxSum: int) -> int:
+        banned_set = set(banned)
+        max_count = 0
+        curr_sum = 0
+        for i in range(1, n+1):
+            if i not in banned_set:
+                curr_sum += i
+                if curr_sum <= maxSum:
+                    max_count += 1
+                else:
+                    return max_count
+        return max_count
+            
+
+
+sol = Solution()
+
+
+print(sol.maxCount(banned = [1,6,5], n = 5, maxSum = 6))
+print(sol.maxCount(banned = [1,2,3,4,5,6,7], n = 8, maxSum = 1))
+print(sol.maxCount(banned = [11], n = 7, maxSum = 50))
