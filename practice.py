471f206ad59683be86a605abed5d43bb608f318b
vsulli <60162352+vsulli@users.noreply.github.com>
2024-12-08 10:48:37 -0600

    #242 valid anagram practice submission

--- practice.py
+++ practice.py
@@ -1,33 +1,22 @@
-# encode and decode strings
+# 242 valid anagram
 
-
-
-from typing import List
 class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
 
-    def encode(self, strs: List[str])-> str:
-        encoded = ""
-        # add #- length of word - word
-        for w in strs:
-            encoded += "#" + str(len(w)) + w
-        return encoded
+        count = [0] * 26
+        for i in range(len(s)):
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
 
-    def decode(self, strs: str)->List[str]:
-        # loop through until pointer is at symbol
-            # copy to result ptr + 2: ptr + 1 + int(string[ptr])
-        res = []
-        ptr = 0
-        while ptr < len(strs):
-            if strs[ptr] == "#":
-                res.append(strs[ptr+2:ptr+2+int(strs[ptr+1])])
-                ptr = ptr+2+int(strs[ptr+1])
-        return res
-
-
+        for c in count:
+            if c != 0:
+                return False
+        return True
+    
 
 sol = Solution()
 
-print(sol.decode(sol.encode(["we","say",":","yes"])))
-# #2we#3say#1:#3yes
-
-print(sol.decode(sol.encode(["neet","code","love","you"])))
+print(sol.isAnagram("anagram", t = "nagaram"))
+print(sol.isAnagram(s = "rat", t = "car"))
