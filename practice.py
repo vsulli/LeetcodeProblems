3c59080c2ed31bf5c68284e4ae47d9b6391bef1f
vsulli <60162352+vsulli@users.noreply.github.com>
2024-12-11 17:09:01 -0600

    practice submission

--- practice.py
+++ practice.py
@@ -1,29 +1,24 @@
-#303 Range Sum Query - Immutable
+# Encode and Decode Strings
 from typing import List
 
-class NumArray:
+class Solution:
+    def encode(self, strs: List[str])-> str:
+        res = ""
+        for s in strs:
+            res += str(len(s)) + "#" + s
+        return res 
 
-    def __init__(self, nums: List[int]):
-        self.nums = nums
-        sum = 0
-        for i in range(1, len(self.nums)):
-            sum = self.nums[i - 1]
-            self.nums[i] = sum + self.nums[i]
+    def decode(self, strs: str)->List[str]:
+        res = []
+        i = 0
 
-    def sumRange(self, left: int, right: int) -> int:
-        # if left is not 0, have to subtract off everything left of that index
-        if left != 0:
-            return self.nums[right] - self.nums[left-1]
-        return self.nums[right]
-        
-
-# Your NumArray object will be instantiated and called as such:
-obj = NumArray([-2, 0, 3, -5, 2, -1])
-# -2, -2, 1, -4, -2, -3 
-print(obj.sumRange(0, 2)) # 1
-
-obj = NumArray([-2, 0, 3, -5, 2, -1])
-print(obj.sumRange(2, 5)) # -1
-
-obj = NumArray([-2, 0, 3, -5, 2, -1])
-print(obj.sumRange(0, 5)) # -3
+        while i < len(strs):
+            j = i
+            while strs[j] != "#":
+                j += 1
+            length = int(strs[i:j])
+            i = j + 1
+            j = i + length
+            res.append(strs[i:j])
+            i = j
+        return res 
