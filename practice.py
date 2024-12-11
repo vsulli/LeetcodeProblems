bbddb671db9e15e521924ea0f25c47798b0f6ac1
vsulli <60162352+vsulli@users.noreply.github.com>
2024-12-10 18:39:30 -0600

    2nd case currently incorrect

--- practice.py
+++ practice.py
@@ -4,13 +4,26 @@ from typing import List
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.array = []
+        self.nums = nums
         self.sum = 0
+        for i in range(len(self.nums)):
+            self.nums[i] = self.sum + self.nums[i]
+            self.sum = self.nums[i]
 
     def sumRange(self, left: int, right: int) -> int:
-        pass
-
+        # if left is not 0, have to subtract that off?
+        if left != 0:
+            return self.nums[right] - self.nums[left]
+        return self.nums[right]
+        
 
 # Your NumArray object will be instantiated and called as such:
-# obj = NumArray(nums)
-# param_1 = obj.sumRange(left,right)
+obj = NumArray([-2, 0, 3, -5, 2, -1])
+# -2, -2, 1, -4, -2, -3 
+print(obj.sumRange(0, 2)) # 1
+
+obj = NumArray([-2, 0, 3, -5, 2, -1])
+print(obj.sumRange(2, 5)) # -1 WRONG
+
+obj = NumArray([-2, 0, 3, -5, 2, -1])
+print(obj.sumRange(0, 5)) # -3
