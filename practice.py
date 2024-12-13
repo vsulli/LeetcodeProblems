a836bc4561dabf82eacedb39aeb07707dfc3b70c
vsulli <60162352+vsulli@users.noreply.github.com>
2024-12-12 19:07:27 -0600

    this solution has about the same efficiency

--- practice.py
+++ practice.py
@@ -3,18 +3,22 @@
 from typing import List
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        groups = {}
-        res = []
+        ans = []
 
-        for s in strs:
-            if tuple(sorted(s)) in groups:
-                groups[tuple(sorted(s))].append(s)
+        # sort letters of each word, if they match dict key,
+        #  then store under same key
+        anagram_dict = {} # sorted_letter: [word]
+
+        for word in strs:
+            if tuple(sorted(word)) in anagram_dict.keys():
+                anagram_dict[tuple(sorted(word))].append(word)
             else:
-                groups[tuple(sorted(s))] = [s]
+                anagram_dict[tuple(sorted(word))] = [word]
         
-        for k in groups:
-            res.append(groups.get(k))
-        return res
+        for value in anagram_dict.values():
+            ans.append(value)
+
+        return ans
 
 sol = Solution()
 
