aa809d0c4ad59cc8dda288b161ad8a2d1692f1ae
vsulli <60162352+vsulli@users.noreply.github.com>
2024-12-14 14:35:17 -0600

    more efficient solution

--- practice.py
+++ practice.py
@@ -2,24 +2,25 @@
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        symbols = {')': '(', '}':'{', ']':'['}
-        
-        my_stack = set()
-        for symbol in s:
-            # if symbol is right, check if left equivalent is in stack
-            if symbol in symbols:
-                popped = my_stack.pop()
-                # check if left is what you get when you look up in stack
-                if popped != symbols.get(symbol):
-                    return False
-            else:
-                my_stack.add(symbol)
-    
-        return len(my_stack) == 0
+        opcl = dict(('()', '[]', '{}'))
+        # Create stack data structure...
+        stack = []
+        # Traverse each charater in input string...
+        for idx in s:
+            # If open parentheses are present, append it to stack...
+            if idx in '([{':
+                stack.append(idx)
+            # If the character is closing parentheses, check that the same type opening parentheses is being pushed to the stack or not...
+            # If not, we need to return false...
+            elif len(stack) == 0 or idx != opcl[stack.pop()]:
+                return False
+        # At last, we check if the stack is empty or not...
+        # If the stack is empty it means every opened parenthesis is being closed and we can return true, otherwise we return false...
+        return len(stack) == 0
     
 sol = Solution()
 
-'''
+
 print(sol.isValid(s = "()")) # check right # true
 
 print(sol.isValid(s = "()[]{}")) # true
@@ -28,6 +29,6 @@ print(sol.isValid(s = "(]")) # false
 
 print(sol.isValid(s = "([])")) # true
 
-'''
+
 
 print(sol.isValid("([])")) # true
