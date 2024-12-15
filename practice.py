56e40c058bba202783623b35b88c9366312c88cb
vsulli <60162352+vsulli@users.noreply.github.com>
2024-12-15 09:37:33 -0600

    for this solution it is better to start at both ends with the pointers

--- practice.py
+++ practice.py
@@ -4,31 +4,25 @@ from typing import List
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        # 1-indexed array, sorted in non-decreasing order
-        # find the two numbers that add up to targest
-        # return the indices of the two numbers
-        # guaranteed solution
-        p1 = 0
-        p2 = 1
-        while numbers[p1] + numbers[p2] != target:
-            # when do you increment the left pointer vs right pointer?
-            sum = numbers[p1] + numbers[p2]
-            if sum > target:
-                p1 -= 1
-            elif sum < target and p2 != len(numbers) - 1:
-                p2 += 1
-            elif sum < target and p2 == len(numbers) - 1:
-                p1 += 1
+        # start on outsides
+        l, r = 0, len(numbers) - 1
 
-        return [p1 + 1, p2 + 1]
+        while l < r:
+            cur_sum = numbers[l] + numbers[r]
+            if cur_sum > target:
+                r -= 1
+            elif cur_sum < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
 
 
 sol = Solution()
 
-print(sol.twoSum(numbers = [2,7,11,15], target = 9))
+print(sol.twoSum(numbers = [2,7,11,15], target = 9)) #indices 1, 2
 
-print(sol.twoSum(numbers = [2,3,4], target = 6))
+print(sol.twoSum(numbers = [2,3,4], target = 6)) # indices 1, 3
 
-print(sol.twoSum(numbers = [-1,0], target = -1))
+print(sol.twoSum(numbers = [-1,0], target = -1)) # 1, 2
 
-print(sol.twoSum(numbers = [2,7,11,15], target = 18))
+print(sol.twoSum(numbers = [2,7,11,15], target = 18)) # 2, 3
