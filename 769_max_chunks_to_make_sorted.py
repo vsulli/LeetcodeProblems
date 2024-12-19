b17dffba140872e359a8f5d03d4df606cd91b751
vsulli <60162352+vsulli@users.noreply.github.com>
2024-12-19 09:29:51 -0600

    python solution using count

--- 769_max_chunks_to_make_sorted.py
+++ 769_max_chunks_to_make_sorted.py
@@ -19,33 +19,11 @@ make to sort the array.
 from typing import List
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
-        # array contains numbers from 0 - (length of array - 1)
-        # want to know how many splits you can have where after you sort those chunks
-        # the concatenated result matches the sorted array
-        # start with as many chunks as possible and reduce?
-        # 10 is largest number of n
-        res = [0] * len(arr)
-        chunks = len(arr)
-        p1 = 0
-        sorted_arr = sorted(arr)
-        if sorted_arr == arr:
-            return len(arr)
-        # where to split to put in order?
-        # where 2nd pointer is at a value that is greater than first?
-        # if rest of array is in order, then you can just count those as individual chunks
-
-        # go left to right, as you see a chunk that is out of order, continue until ascending
-        # then change all those values and count as x chunks subtracted (length of section reversed)
-        p2 = 1
-        while res != sorted(arr):
-            while p2 < len(arr) and arr[p2] < arr[p1]:
-                p2 += 1
-            res[p1:p2] = arr[p1:p2]
-            chunks -= (p2 - p1)
-
-            
-
-        return chunks
+        cnt=diff=0
+        for i, x in enumerate(arr):
+            diff+=x-i
+            cnt+=(diff==0)
+        return cnt
 
 sol = Solution()
 
