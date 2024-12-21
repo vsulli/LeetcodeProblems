af0d4f6c8572f135d5bd1cc1928e035ceeea99a0
vsulli <60162352+vsulli@users.noreply.github.com>
2024-12-21 10:01:39 -0600

    solution looping from left and right

--- practice.py
+++ practice.py
@@ -1,22 +1,24 @@
 # 238 Product of Array Except SElf
 
 from typing import List
-import math
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # o(n) time means that you only loop through the array once
-        # can't use division operator
-        # get product of array except for that index
-        # calculate all multiplied, find difference for that index?
-        products = [0] * len(nums)
-        p = 0
-
+        # loop through twice
+        # get product from left and then product from right
+        output = [1] * len(nums)
+        
+        left = 1
         for i in range(len(nums)):
-            # multiply all the numbers of product from left to right of pointer?
-            products[i] = math.prod(nums[0:i]) * math.prod(nums[i+1:])
-            p+= 1
-        return products
+            output[i] *= left
+            left *= nums[i]
+        
+        right = 1
+        for i in range(len(nums) - 1, -1, -1):
+            output[i] *= right
+            right *= nums[i]
+    
+        return output        
 
 
 sol = Solution()
