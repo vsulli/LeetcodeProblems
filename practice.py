36c8bc34f2bf5be7ff5c7b397fbf2ce5d73182bc
vsulli <60162352+vsulli@users.noreply.github.com>
2024-12-22 08:06:44 -0600

    2nd loop from right in range(last index, to -1 which is never reached, going by -1)

--- practice.py
+++ practice.py
@@ -4,21 +4,20 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # loop through twice
-        # get product from left and then product from right
         output = [1] * len(nums)
-        
+
         left = 1
         for i in range(len(nums)):
             output[i] *= left
             left *= nums[i]
         
+
         right = 1
         for i in range(len(nums) - 1, -1, -1):
             output[i] *= right
             right *= nums[i]
-    
-        return output        
+                    
+        return output 
 
 
 sol = Solution()
