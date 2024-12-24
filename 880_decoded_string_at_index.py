bcab355c9161293b82b5ca0b415c90930f7fe66f
vsulli <60162352+vsulli@users.noreply.github.com>
2024-12-24 16:29:42 -0600

    infinite loop for last case with one string and all numbers?

---
+++ 880_decoded_string_at_index.py
@@ -0,0 +1,42 @@
+'''
+Decoded String at Index
+Leetcode # 880
+vsulli
+24 December 2024
+
+You are given an encoded string s. 
+To decode the string to a tape, 
+the encoded string is read one character 
+at a time and the following steps are taken:
+
+If the character read is a letter, 
+that letter is written onto the tape.
+If the character read is a digit d, 
+the entire current tape is repeatedly 
+written d - 1 more times in total.
+Given an integer k, return the kth 
+letter (1-indexed) in the decoded string.
+
+'''
+
+class Solution:
+    def decodeAtIndex(self, s: str, k: int) -> str:
+        # want to return the decoded string at index k (k - 1 index)
+        decoded = ""
+        for c in s:
+            if c.isalpha():
+                decoded += c
+            else:
+                # case where that char is number
+                # add to current string - the current string x k - 1
+                duplicated =  decoded * (int(c) - 1)
+                decoded += duplicated
+        return decoded
+
+sol = Solution()
+
+print(sol.decodeAtIndex(s = "leet2code3", k = 10))
+
+print(sol.decodeAtIndex(s = "ha22", k = 5))
+
+print(sol.decodeAtIndex(s = "a2345678999999999999999", k = 1))
--- practice.py
+++ practice.py
@@ -1,26 +1,3 @@
-# 238 Product of Array Except SElf
+string = "leet"
 
-from typing import List
-
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        output = [1] * len(nums)
-
-        left = 1
-        for i in range(len(nums)):
-            output[i] *= left
-            left *= nums[i]
-        
-
-        right = 1
-        for i in range(len(nums) - 1, -1, -1):
-            output[i] *= right
-            right *= nums[i]
-                    
-        return output 
-
-
-sol = Solution()
-print(sol.productExceptSelf(nums = [1,2,3,4]))
-
-print(sol.productExceptSelf(nums = [-1, 1, 0, -3, 3]))
+print(string*2)
