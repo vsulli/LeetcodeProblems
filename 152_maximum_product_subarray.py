61a683b43045e0bc540aa9df8cdbf86b77070dcc
vsulli <60162352+vsulli@users.noreply.github.com>
2024-12-25 08:41:40 -0600

    initial commit

---
+++ 152_maximum_product_subarray.py
@@ -0,0 +1,24 @@
+'''
+Maximum Product Subarray
+Leetcode # 152
+vsulli
+25 December 2024
+
+Given an integer array nums, find a subarray that
+has the largest product, and return the product.
+
+The test cases are generated so that the answer will fit
+in a 32-bit integer.
+
+'''
+from typing import List
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        pass
+
+sol = Solution()
+
+print(sol.maxProduct(nums = [2,3,-2,4]))
+
+print(sol.maxProduct(nums = [-2,0,-1]))
