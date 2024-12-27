251bdabafb1f2b73799f864b044303f2f2cf0056
vsulli <60162352+vsulli@users.noreply.github.com>
2024-12-27 17:02:11 -0600

    lru cache - initialize the cache with empty nodes left and right

--- practice.py
+++ practice.py
@@ -3,6 +3,15 @@
 class Node:
     def __init__(self, key, val):
         self.key, self.val = key, val
-        self.prev, self.next = None, None
+        self.prev, self.next = None
 
 
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {} # key: node
+
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left 
+
+        
