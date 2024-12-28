895623f61a245f7d8f954bbb456be6c951733e45
vsulli <60162352+vsulli@users.noreply.github.com>
2024-12-28 09:43:55 -0600

    full code LRU cache - learn insert, remove

--- practice.py
+++ practice.py
@@ -14,4 +14,39 @@ class LRUCache:
         self.left, self.right = Node(0, 0), Node(0, 0)
         self.left.next, self.right.prev = self.right, self.left 
 
+    def remove(self, node):
+        # get node's prev and next
+        prev, nxt = node.prev, node.next
+
+        # updating pointers of one behind and one in front
+        prev.next, nxt.prev = nxt, prev 
+
+    # insert at right
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node 
+        node.next, node.prev = nxt, prev
+
+    def get(self, key: int):
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            # update to MRU
+            return self.cache[key].val
+        return -1 
+
+    def put(self, key: int, value: int)-> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
         
+        self.cache[key] = Node(key, value)
+        # insert node into cache
+        self.insert(self.cache[key])
+        # check capacity
+        if len(self.cache) > self.cap:
+            # remove from list and delete LRU from hashmap
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
+
