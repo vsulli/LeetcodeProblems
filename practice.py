884caa00ce59c4228f27ff2e37b7cafa4ab2b6ff
vsulli <60162352+vsulli@users.noreply.github.com>
2024-12-30 18:37:59 -0600

    insert and remove attempt

--- practice.py
+++ practice.py
@@ -3,25 +3,27 @@
 class Node:
     def __init__(self, key, val):
         self.key, self.val = key, val
-        self.next = self.prev = None
+        self.prev = self.next = None
 
 class LRUCache:
     def __init__(self, capacity: int):
         self.cap = capacity
-        self.cache = {}
 
-        self.left, self.right = Node(0,0), Node(0,0)
+        self.left, self.right = Node(0, 0), Node(0, 0)
         self.left.next, self.right.prev = self.right, self.left
-    
-    def remove(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev  
 
-    # insert at right
+    # insert linked list
     def insert(self, node):
-        prev, nxt = self.right.prev, self.right
-        prev.next = nxt.prev = node 
-        node.next, node.prev = nxt, prev
+        prv, nxt = self.right.prev, self.right 
+        node.left = prv
+        node.right = nxt 
+        self.right = node
+
+    # remove linked list
+    def remove(self, node):
+        prv, nxt = node.left, node.right
+        prv.next = nxt
+        nxt.prev = prv 
 
     def get(self, key: int):
         if key in self.cache:
