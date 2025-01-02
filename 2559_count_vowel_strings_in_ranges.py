0f41c9ce692f94c07939db38eee74058927a7816
vsulli <60162352+vsulli@users.noreply.github.com>
2025-01-02 16:42:52 -0600

    initial commit #2559

---
+++ 2559_count_vowel_strings_in_ranges.py
@@ -0,0 +1,10 @@
+from typing import List
+class Solution:
+    def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
+        pass
+
+sol = Solution()
+
+print(sol.vowelStrings(words = ["aba","bcb","ece","aa","e"], queries = [[0,2],[1,4],[1,1]]))
+
+print(sol.vowelStrings(words = ["a","e","i"], queries = [[0,2],[0,1],[2,2]]))
