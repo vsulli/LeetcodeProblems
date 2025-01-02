b81382aeca3b2a0306239a59b2c7f4b4e2207b9e
vsulli <60162352+vsulli@users.noreply.github.com>
2025-01-02 16:49:01 -0600

    my solution working

--- 2559_count_vowel_strings_in_ranges.py
+++ 2559_count_vowel_strings_in_ranges.py
@@ -1,7 +1,24 @@
 from typing import List
 class Solution:
     def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
-        pass
+        vowels = ['a', 'e', 'i', 'o', 'u']
+        # add all words to dictionary with index as key?
+        res = []
+        word_dict = {}
+        for i in range(len(words)):
+            word_dict[i] = words[i]
+
+        for q in queries:
+            count = 0
+            for i in range(q[0], q[1] + 1):
+                # if starts and ends with a vowel or length 1 and vowel
+                # add to result
+                if len(word_dict[i]) == 1 and word_dict[i] in vowels:
+                    count += 1
+                elif word_dict[i][0] in vowels and word_dict[i][-1] in vowels:
+                    count += 1
+            res.append(count)
+        return res
 
 sol = Solution()
 
