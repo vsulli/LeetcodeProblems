c329f5b7f55118cc0daf9a1b2e7fd29a0cc0b4d0
vsulli <60162352+vsulli@users.noreply.github.com>
2025-01-02 16:33:04 -0600

    incorrect last two functions?

--- practice.py
+++ practice.py
@@ -1,4 +1,3 @@
-
 # LRU Cache
 
 class Node:
@@ -14,14 +13,32 @@ class LRUCache:
         self.left, self.right = Node(0, 0), Node(0, 0)
         self.left.next, self.right.prev = self.right, self.left 
 
-    # insert into linked list
-    def insert(self, node):
-        prv, nxt = self.right.prev, self.right 
-        node.prev, node.next = prv,nxt
-        prv.next = nxt.prev = node 
-
-    # remove from linked list
-    def remove(self, node):
+    def remove(self, node): # linked list
         prv, nxt = node.prev, node.next 
         prv.next, nxt.prev = nxt, prv 
+
+    def insert(self, node): # linked list
+        prv, nxt = self.right.prev, self.right
+        node.prev, node.next = prv, nxt
+        prv.next = nxt.prev = node
+
+    def get(self, key: int): 
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key]
+        return -1
+
+    def put(self, key: int, value: int):
+        if key in self.cache:
+            self.remove(self.cache[key]) # remove from linked list
+        self.cache[key] = value  # insert into cache
+        self.insert(key) # insert into linked list
+        if len(self.cache) > self.cap:
+            # get lru
+            lru = self.left 
+            # remove lru from cache and linked list
+            self.remove(lru.val)
+            del self.cache[lru]
+
 
