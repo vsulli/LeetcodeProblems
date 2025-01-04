29cadd59ded6a85122bd9172483430a9c1812f58
vsulli <60162352+vsulli@users.noreply.github.com>
2025-01-03 18:16:10 -0600

    remember what to delete from cache

--- practice.py
+++ practice.py
@@ -1,28 +1,27 @@
-# LRU Cache
-
 class Node:
     def __init__(self, key, val):
         self.key, self.val = key, val
+
         self.prev = self.next = None
 
-class LRUCache:
+class LRUCache: 
     def __init__(self, capacity: int):
         self.cap = capacity
         self.cache = {}
 
         self.left, self.right = Node(0, 0), Node(0, 0)
-        self.left.next, self.right.prev = self.right, self.left 
+        self.left.next, self.right.prev = self.right, self.left
 
-    def remove(self, node): # linked list
-        prv, nxt = node.prev, node.next 
-        prv.next, nxt.prev = nxt, prv 
-
-    def insert(self, node): # linked list
+    def insert(self, node):
         prv, nxt = self.right.prev, self.right
-        node.prev, node.next = prv, nxt
+        node.prev, node.next = prv, nxt 
         prv.next = nxt.prev = node
 
-    def get(self, key: int): 
+    def remove(self, node):
+        prv, nxt = node.prev, node.next
+        prv.next, nxt.prev = nxt, prv
+        
+    def get(self, key: int):
         if key in self.cache:
             self.remove(self.cache[key])
             self.insert(self.cache[key])
@@ -32,13 +31,14 @@ class LRUCache:
     def put(self, key: int, value: int):
         if key in self.cache:
             self.remove(self.cache[key]) # remove from linked list
-        self.cache[key] = Node(key, value)  # insert into cache
-        self.insert(self.cache[key]) # insert into linked list
+        
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
         if len(self.cache) > self.cap:
-            # get lru
-            lru = self.left.next
-            # remove lru from cache and linked list
-            self.remove(lru)
-            del self.cache[lru.key]
+            lru = self.left.next 
+            # remove from linked list and cache
+            self.remove(lru) # TODO
+            del self.cache[lru.key] # TODO
+
 
 
