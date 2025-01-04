aa6a53596484be0e1b970c5b5481d4d3b4abe378
vsulli <60162352+vsulli@users.noreply.github.com>
2025-01-04 12:59:32 -0600

    practice submission - remember that you're deleting the node's key from the cache itself - del self.cache[lru.key]

--- practice.py
+++ practice.py
@@ -1,26 +1,27 @@
+# lru cache
+
 class Node:
     def __init__(self, key, val):
         self.key, self.val = key, val
-
         self.prev = self.next = None
 
-class LRUCache: 
+class LRUCache:
     def __init__(self, capacity: int):
-        self.cap = capacity
+        self.cap = capacity 
         self.cache = {}
 
         self.left, self.right = Node(0, 0), Node(0, 0)
-        self.left.next, self.right.prev = self.right, self.left
-
-    def insert(self, node):
-        prv, nxt = self.right.prev, self.right
-        node.prev, node.next = prv, nxt 
-        prv.next = nxt.prev = node
+        self.left.next, self.right.prev = self.right, self.left 
 
     def remove(self, node):
         prv, nxt = node.prev, node.next
-        prv.next, nxt.prev = nxt, prv
-        
+        prv.next, nxt.prev = nxt, prv 
+
+    def insert(self, node):
+        prv, nxt = self.right.prev, self.right 
+        node.prev, node.next = prv, nxt 
+        prv.next = nxt.prev = node 
+
     def get(self, key: int):
         if key in self.cache:
             self.remove(self.cache[key])
@@ -30,15 +31,15 @@ class LRUCache:
 
     def put(self, key: int, value: int):
         if key in self.cache:
-            self.remove(self.cache[key]) # remove from linked list
-        
+            self.remove(self.cache[key])
         self.cache[key] = Node(key, value)
         self.insert(self.cache[key])
         if len(self.cache) > self.cap:
             lru = self.left.next 
-            # remove from linked list and cache
-            self.remove(lru) # TODO
-            del self.cache[lru.key] # TODO
+            self.remove(lru)
+            del self.cache[lru.key]
+
+
 
 
 
