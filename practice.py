8ee1f84ea2f9ce9a5370be88e80a3f6c45b9b000
vsulli <60162352+vsulli@users.noreply.github.com>
2025-01-05 09:50:16 -0600

    insert - self.right.prev not self.right.left

--- practice.py
+++ practice.py
@@ -1,5 +1,3 @@
-# lru cache
-
 class Node:
     def __init__(self, key, val):
         self.key, self.val = key, val
@@ -11,34 +9,34 @@ class LRUCache:
         self.cache = {}
 
         self.left, self.right = Node(0, 0), Node(0, 0)
-        self.left.next, self.right.prev = self.right, self.left 
+        self.left.next, self.right.prev = self.right, self.left
+    
+    def insert(self, node):
+        prv, nxt = self.right.prev, self.right
+        node.prev, node.next = prv, nxt 
+        prv.next = nxt.prev = node 
 
     def remove(self, node):
         prv, nxt = node.prev, node.next
-        prv.next, nxt.prev = nxt, prv 
-
-    def insert(self, node):
-        prv, nxt = self.right.prev, self.right 
-        node.prev, node.next = prv, nxt 
-        prv.next = nxt.prev = node 
+        prv.next, nxt.prev = nxt, prv
 
     def get(self, key: int):
         if key in self.cache:
             self.remove(self.cache[key])
             self.insert(self.cache[key])
             return self.cache[key].val
-        return -1
+        return -1 
 
     def put(self, key: int, value: int):
         if key in self.cache:
             self.remove(self.cache[key])
+        
         self.cache[key] = Node(key, value)
         self.insert(self.cache[key])
         if len(self.cache) > self.cap:
-            lru = self.left.next 
+            lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
-
 
 
 
