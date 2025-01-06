fdb2e51bd22ebd2e60e97282997fa22ab45bcc5f
vsulli <60162352+vsulli@users.noreply.github.com>
2025-01-06 06:15:10 -0600

    remember this line self.remove(lru)

--- practice.py
+++ practice.py
@@ -1,6 +1,39 @@
-queries = [[0,2],[1,4],[1,1]]
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
 
-prefix_sum = [1, 1, 2, 3, 4]
+        self.prev = self.next = None
 
-for q in queries:
-    print(q[1])
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {}
+
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
+
+    def insert(self, node):
+        prv, nxt = self.right.prev, self.right
+        node.prev, node.next = prv, nxt 
+        prv.next = nxt.prev = node
+
+    def remove(self, node):
+        prv, nxt = node.prev, node.next
+        prv.next, nxt.prev = nxt, prv
+
+    def get(self, key: int):
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
+
+    def put(self, key: int, value: int):
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+        if len(self.cache) > self.cap:
+            lru = self.left.next 
+            self.remove(lru)
+            del self.cache[lru.key]
