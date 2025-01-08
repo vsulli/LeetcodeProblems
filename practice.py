47dd4e27191b2f12b6351aebd738a71952f49f7d
vsulli <60162352+vsulli@users.noreply.github.com>
2025-01-08 17:38:32 -0600

    practice submission for top k frequent elements

--- practice.py
+++ practice.py
@@ -1,24 +1,25 @@
-#2559 
+# 347 Top K Frequent Elements
+
 from typing import List
+
 class Solution:
-    def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
-        vowels = {"a", "e", "i", "o", "u"}
-        count = 0
-        prefix_sum = [0] * len(words)
-        ans = [0] * len(queries)
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        num_count = {}
+        for n in nums:
+            if n in num_count:
+                num_count[n] += 1
+            else:
+                num_count[n] = 1
+        sorted_num_count = sorted(num_count.items(), key=lambda items:items[1], reverse=True)
+        res = []
+        for i, n in enumerate(sorted_num_count):
+            res.append(n[0])
+            if i == k - 1:
+                return res
 
-        for i in range(len(words)):
-            if words[i][0] and words[i][-1] in vowels:
-                count += 1
-            prefix_sum[i] =  count
-
-        for i in range(len(queries)):
-            ans[i] = prefix_sum[queries[i][1]] - (0 if queries[i][0] == 0 else queries[i][0] - 1)
-        return ans
-          
 
 sol = Solution()
 
-print(sol.vowelStrings(words = ["aba","bcb","ece","aa","e"], queries = [[0,2],[1,4],[1,1]]))
+print(sol.topKFrequent(nums = [1,1,1,2,2,3], k = 2))
 
-print(sol.vowelStrings(words = ["a","e","i"], queries = [[0,2],[0,1],[2,2]]))
+print(sol.topKFrequent(nums = [1], k = 1))
