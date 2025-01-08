1719937d54beb4e2a859d6b76623e974549c9f81
vsulli <60162352+vsulli@users.noreply.github.com>
2025-01-08 17:43:04 -0600

    need to review lambda again and sorting

--- practice.py
+++ practice.py
@@ -4,15 +4,16 @@ from typing import List
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        num_count = {}
+        count = {}
         for n in nums:
-            if n in num_count:
-                num_count[n] += 1
+            if n in count:
+                count[n] += 1
             else:
-                num_count[n] = 1
-        sorted_num_count = sorted(num_count.items(), key=lambda items:items[1], reverse=True)
+                count[n] = 1
+        
+        s_count = sorted(count.items(), key=lambda items:items[1], reverse=True)
         res = []
-        for i, n in enumerate(sorted_num_count):
+        for i,n in enumerate(s_count):
             res.append(n[0])
             if i == k - 1:
                 return res
