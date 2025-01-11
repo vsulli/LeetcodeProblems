80158826bd0c236dd6d9ccf3011b9c0853f377bf
vsulli <60162352+vsulli@users.noreply.github.com>
2025-01-11 07:35:30 -0600

    insert into a bst practice

--- practice.py
+++ practice.py
@@ -1,21 +1,15 @@
-
-from typing import List
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        for n in nums:
-            if n in count:
-                count[n] += 1
-            else:
-                count[n] = 1
-        
-        res = []
-        s_count = sorted(count.items(), key=lambda items:items[1], reverse=True)
-
-        for i, n in enumerate(s_count):
-            res.append(n[0])
-            if i == k - 1:
-                return res
-                
-sol = Solution()
-
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return TreeNode(val)
+        elif val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        else:
+            root.right = self.insertIntoBST(root.right, val)
+        return root
