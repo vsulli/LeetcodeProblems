e92596f6a90198e25aaf37b3dc9049f46c30eff1
vsulli <60162352+vsulli@users.noreply.github.com>
2025-01-18 08:58:45 -0600

    infinite loop

--- practice.py
+++ practice.py
@@ -1,15 +1,26 @@
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-from typing import Optional
+from typing import List
 class Solution:
-    def insertIntoBST(self, root: Optional[TreeNode], val: int)->Optional[TreeNode]:
-        if not root:
-            return TreeNode(val)
-        elif val < root.val:
-            root.left = self.insertIntoBST(root.left, val)
-        elif val > root.val:
-            root.right = self.insertIntoBST(root.right, val)
-        return root
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # start with two pointers in the middle?
+        # move right if need greater, move left if need smaller?
+        l = (len(numbers) // 2 ) - 1
+        r = l + 1
+        curr_sum = numbers[l] + numbers[r]
+        while curr_sum != target:
+            if curr_sum < target and r < len(numbers):
+                # need to increase value
+                r += 1
+            elif curr_sum > target and l > len(numbers):
+                l -= 1
+            curr_sum = numbers[l] + numbers[r]
+        return [l, r]
+
+
+
+sol =  Solution()
+
+print(sol.twoSum(numbers = [2,7,11,15], target = 9))
+
+print(sol.twoSum(numbers = [2,3,4], target = 6))
+
+print(sol.twoSum(numbers = [-1,0], target = -1))
