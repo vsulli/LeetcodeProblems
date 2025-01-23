f076e40098c8b2123680c3987de6485d11a0cf62
vsulli <60162352+vsulli@users.noreply.github.com>
2025-01-22 19:07:43 -0600

    product of array except self solution - 97.65% runtime

--- practice.py
+++ practice.py
@@ -1,45 +1,26 @@
-# lc 146 - lru cache
+# lc 238 - product of array except self
 
+from typing import List
+class Solution:
+    def productExceptSelf(self, nums: List[int])->List[int]:
+        res = [1] * len(nums)
 
-class Node:
-    def __init__(self, key, val):
-        self.key = key
-        self.val = val
-        self.prev = self.next = None
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
 
-class LRUCache:
-
-    def __init__(self, capacity: int):
-        self.cap = capacity
-        self.cache = {}
-
-        self.left, self.right = Node(0, 0), Node(0, 0)
-        self.left.next, self.right.prev = self.right, self.left
-
-    def remove(self, node):
-        prv, nxt = node.prev, node.next
-        prv.next, nxt.prev = nxt, prv
-
-    def insert(self, node):
-        prv, nxt = self.right.prev, self.right
-        node.prev, node.next = prv, nxt
-        prv.next = nxt.prev = node
-
-    def get(self, key: int)->int:
-        if key in self.cache:
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
-            return self.cache[key].val
-        return -1
-
-    def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.remove(self.cache[key])
-        self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
-        if len(self.cache) > self.cap:
-            lru = self.left.next
-            self.remove(lru)
-            del self.cache[lru.key]
-
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
         
+        return res
+
+
+sol = Solution()
+
+print(sol.productExceptSelf(nums = [1,2,3,4]))
+
+print(sol.productExceptSelf(nums = [-1,1,0,-3,3]))
+
