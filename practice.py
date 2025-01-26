5230535fd4dce12df2243a43fb41b9c573861bd7
vsulli <60162352+vsulli@users.noreply.github.com>
2025-01-26 09:35:58 -0600

    need to re-learn delete node bst

--- practice.py
+++ practice.py
@@ -1,8 +1,8 @@
 # 146 LRU Cache
+
 class Node:
-    def __init__(self, key, val):
-        self.key = key
-        self.val = val
+    def __init__(self, key: int, val: int):
+        self.key, self.val = key, val
         self.prev = self.next = None
 
 class LRUCache:
@@ -12,35 +12,26 @@ class LRUCache:
         self.cache = {}
 
         self.left, self.right = Node(0, 0), Node(0, 0)
-        self.left.next, self.right.prev = self.right, self.left
+        self.left.next, self.right.prev = self.right, self.left 
 
     def remove(self, node):
         prv, nxt = node.prev, node.next
         prv.next, nxt.prev = nxt, prv
 
     def insert(self, node):
-        prv, nxt = self.right.prev, self.right
-        node.prev, node.next = prv, nxt
+        prv, nxt = self.right.prev, self.right 
+        node.prev, node.next = prv, nxt 
         prv.next = nxt.prev = node
 
-    def get(self, key: int):
-        if key in self.cache:
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
-            return self.cache[key].val
-        return -1
 
-    def put(self, key: int, value: int):
-        if key in self.cache:
-            self.remove(self.cache[key])
-        
-        self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
-        if len(self.cache) > self.cap:
-            lru = self.left.next
-            self.remove(lru)
-            del self.cache[lru.key]
-            # find lru
-            # remove from linked list
-            # remove from cache
+    def get(self, key: int) -> int:
+        pass
 
+    def put(self, key: int, value: int) -> None:
+        pass
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
