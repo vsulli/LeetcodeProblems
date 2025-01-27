5d1255c79a740a3ab74560ff7ea92a2165a10b1c
vsulli <60162352+vsulli@users.noreply.github.com>
2025-01-27 17:25:09 -0600

    need to remember next pointer for lru

--- practice.py
+++ practice.py
@@ -3,35 +3,34 @@
 class Node:
     def __init__(self, key: int, val: int):
         self.key, self.val = key, val
+
         self.prev = self.next = None
 
 class LRUCache:
-
     def __init__(self, capacity: int):
         self.cap = capacity
         self.cache = {}
 
         self.left, self.right = Node(0, 0), Node(0, 0)
         self.left.next, self.right.prev = self.right, self.left 
-
+    
     def remove(self, node):
         prv, nxt = node.prev, node.next
         prv.next, nxt.prev = nxt, prv
 
     def insert(self, node):
-        prv, nxt = self.right.prev, self.right 
+        prv, nxt = self.right.prev, self.right
         node.prev, node.next = prv, nxt 
-        prv.next = nxt.prev = node
+        prv.next = nxt.prev = node 
 
 
-    def get(self, key: int) -> int:
+    def get(self, key: int):
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key]
+        return -1
+
+    def put(self, key: int, value: int):
         pass
 
-    def put(self, key: int, value: int) -> None:
-        pass
-
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
